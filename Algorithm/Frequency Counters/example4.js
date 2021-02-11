0e6cae43351150bb2c333449ca7c329f2aff4cc8
Sun flower <MLIU2@LTU.EDU>
2021-02-11 17:32:58 -0600

    Create example4.js

---
+++ Algorithm/Frequency Counters/example4.js
@@ -0,0 +1,21 @@
+/*
+  implement a function called hasDuplocated which accepts a varible number of integer arguments and returns 
+  true if there are duplocates in the arguments.
+
+  should be solved in O(n) or better
+
+  eg:
+  hasDuplicates(1,2,3,4,5,6)    false
+  hasDuplicates(1,2,3,3,4,5)    true
+*/
+
+const hasDuplicates = (...args) => {
+  let lookUp = {};
+
+  const inputs = Array.from(args);
+  for(let val of inputs){
+    if(lookUp[val] >=1) return true
+    else lookUp[val] = (lookUp[val]||0) + 1
+  }
+  return false
+}
