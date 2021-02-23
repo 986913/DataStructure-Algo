a13b8c0b29f8c622235cefd8384290016cfdaa2a
Sun flower <MLIU2@LTU.EDU>
2021-02-22 21:57:39 -0600

    Update example4.js

--- Algorithm/Sliding Window/example4.js
+++ Algorithm/Sliding Window/example4.js
@@ -8,15 +8,35 @@ eg:
 */
 
 // 暴力法：
+// const maxSum = (arr, k) => {
+//   let maxSum = 0;
+
+//   for (let i = 0; i < arr.length - k + 1; i++) {
+//     let tmpSum = 0;
+//     for (let j = 0; j < k; j++) {
+//       tmpSum += arr[i + j];
+//     }
+//     maxSum = Math.max(maxSum, tmpSum);
+//   }
+//   return maxSum;
+// };
+// console.log(maxSum([100, 200, 300, 400], 2));
+
+
+
+// sliding window:
 const maxSum = (arr, k) => {
   let maxSum = 0;
+  // 第一个窗口
+  for (let i = 0; i < k; i++) {
+    maxSum += arr[i];
+  }
 
-  for (let i = 0; i < arr.length - k + 1; i++) {
-    let tmpSum = 0;
-    for (let j = 0; j < k; j++) {
-      tmpSum += arr[i + j];
-    }
-    maxSum = Math.max(maxSum, tmpSum);
+  let tmpSum = maxSum;
+  //开始滑动
+  for (let i = k; i < arr.length; i++) {
+    tmpSum = tmpSum + arr[i] - arr[i - k];
+    maxSum = Math.max(tmpSum, maxSum);
   }
   return maxSum;
 };
