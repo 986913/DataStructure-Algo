f7740b5dbf76d75ee7830073d36a10d576815f25
Sun flower <MLIU2@LTU.EDU>
2021-02-25 21:25:33 -0600

    Update example1.js

--- Algorithm/Recursion/example1.js
+++ Algorithm/Recursion/example1.js
@@ -3,3 +3,21 @@
     1. Base case  （conditon when to stop）
     2. Different input
 */
+
+/*
+
+wirte a fuction called countDown, input is a number;
+output is print all number, the last one is "all done" 
+
+eg: countDown(4)  4 3 2 1 'all done'
+ */
+
+//迭代
+const countDown = n => {
+  for(let i=n; i>0; i--){
+    console.log(i)
+  }
+  console.log('all done')
+}
+
+// recursion:
