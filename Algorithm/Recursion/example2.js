ffb5c2dfb28df5d788a78eb7c6d45f4903aae790
Sun flower <MLIU2@LTU.EDU>
2021-02-28 18:30:29 -0600

    Update example2.js

--- Algorithm/Recursion/example2.js
+++ Algorithm/Recursion/example2.js
@@ -1,8 +1,21 @@
+/*
+eg: sumRange(4)  // 10  解释：4+3+2+1=10
+*/
+
+//迭代：
+const sumRange = n => {
+  let sum =0;
+  for(let i=1; i<=n; i++){
+    sum+=i;
+  }
+  return sum;
+}
+
+// Recurrion:  递归
 const sumRange = n => {
   if(n===1) return 1;
   return n+sumRange(n-1);
 }
-
 
 sumRange(3);   // 6
 sumRange(4);   // 10
