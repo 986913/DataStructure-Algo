eea51b22bf47da2af6d5293be43d8063bcfdf29d
Sun flower <MLIU2@LTU.EDU>
2021-02-28 18:36:10 -0600

    Create example3.js

---
+++ Algorithm/Recursion/example3.js
@@ -0,0 +1,20 @@
+/*
+
+wirte a fuction called factorial, input is a number;
+output is the Factorial of input
+
+eg: factorial(4) // 24   解释：4x3x2x1=24
+    factorial(5) // 120  解释：5x4x3x2x1=120
+*/
+
+
+// 迭代法：
+// const factorial = n => {
+//   let result = 1;
+//   for(i=1; i<=n; i++){
+//     result = result * i
+//   }
+//   return result;
+// }
+
+factorial(5)
