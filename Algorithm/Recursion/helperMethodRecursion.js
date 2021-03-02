73aea7c9c45e2cae8dd0bd51ad112d19d10b8e29
Sun flower <MLIU2@LTU.EDU>
2021-03-01 22:03:55 -0600

    Create helperMethodRecursion.js

---
+++ Algorithm/Recursion/helperMethodRecursion.js
@@ -0,0 +1,14 @@
+/*
+  Helper method recursion is a design pattern of recursion:
+
+  模板:
+  function outer(input){
+    var outerScopedVariable = [];
+    function helper(helperInput){
+      // modify the outerScopedVariable
+    }
+    helper(input);
+    return outerScopedVariable
+  }
+  
+*/
