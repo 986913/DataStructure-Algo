a517a4f2a42bb67e2cb0aa363fe368373ff46971
Sun flower <MLIU2@LTU.EDU>
2021-03-01 22:28:08 -0600

    Create pureRecursion.js

---
+++ Algorithm/Recursion/pureRecursion.js
@@ -0,0 +1,19 @@
+/*
+  use other way(pure recursion) to address this issue:   helperMethodRecursion.js
+
+*/
+
+const collectOddValues = arr => {
+  let result = [];
+
+  if(arr.length === 0) return result;
+
+  if(arr[0]%2 !==0 ){
+    result.push(arr[0])
+  }
+  result=result.concat(collectOddValues(arr.slice(1)))
+
+  return result;
+}
+
+console.log(collectOddValues([1,2,3,4,5]))  //[1,3,5]
