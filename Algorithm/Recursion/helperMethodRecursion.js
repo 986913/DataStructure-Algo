876a37f25cdfa101dfaa80553f8b5fe03eadf44c
Sun flower <MLIU2@LTU.EDU>
2021-03-01 22:11:38 -0600

    Update helperMethodRecursion.js

--- Algorithm/Recursion/helperMethodRecursion.js
+++ Algorithm/Recursion/helperMethodRecursion.js
@@ -13,3 +13,30 @@
   }
 
 */
+
+
+
+/* for example:
+
+  try to collect all of odd values in an array
+ */
+
+const collectOddValues = (arr) => {
+  let result = [];
+
+  const helper = (helperInput) => {
+    if(helperInput.length === 0) return;
+    //modify the result;
+    if(helperInput[0]%2 !== 0){
+      result.push(helperInput[0])
+    }
+    helper(helperInput.slice(1))
+
+  }
+
+  helper(arr);
+  return result;
+
+}
+
+console.log(collectOddValues([1,2,3,4,5]))  // [1,3,5]
