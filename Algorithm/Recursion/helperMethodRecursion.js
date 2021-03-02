1fa4d6713e7e2385b2a51161aa97836ff393fc31
Sun flower <MLIU2@LTU.EDU>
2021-03-01 22:04:40 -0600

    Update helperMethodRecursion.js

--- Algorithm/Recursion/helperMethodRecursion.js
+++ Algorithm/Recursion/helperMethodRecursion.js
@@ -5,10 +5,11 @@
   function outer(input){
     var outerScopedVariable = [];
     function helper(helperInput){
-      // modify the outerScopedVariable
+      // modify the outerScopedVariable;
+      helper(helperInput--)
     }
     helper(input);
     return outerScopedVariable
   }
-  
+
 */
