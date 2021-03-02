bf12463da38d1e33564ce2037309537a36adca74
Sun flower <MLIU2@LTU.EDU>
2021-03-01 22:32:33 -0600

    Update pureRecursion.js

--- Algorithm/Recursion/pureRecursion.js
+++ Algorithm/Recursion/pureRecursion.js
@@ -1,6 +1,10 @@
 /*
   use other way(pure recursion) to address this issue:   helperMethodRecursion.js
 
+  pure recursion tips:
+   1. For arrays, use method like slice, the spread operator, and concat that make copies of arrays so you don't mutate them
+   2. For strings, as they are immutable, so you'll need to use methods like slice, substr, substring to make copies of strings
+   3. For objects, use Object.assign, or te spread operator to make copies of objects
 */
 
 const collectOddValues = arr => {
