391b4bc2e37d5294c227216bafe9dde6abf2365b
Sun flower <MLIU2@LTU.EDU>
2021-03-07 14:29:45 -0600

    Create 1137.js

---
+++ Leetcode/1137.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+
+
+let memoization = [0, 1, 1]
+  
+var tribonacci = function(n) {
+  if (memoization[n] != undefined) return memoization[n];
+  memoization[n] = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3)
+  return memoization[n]
+};
+
