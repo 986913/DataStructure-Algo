58210666fd6c9ca33a3999f11e129f0b1a111b01
Sun flower <MLIU2@LTU.EDU>
2021-03-08 20:48:39 -0600

    Update bubbleSort.js

--- Algorithm/Sort Algorithm/Bubble Sort/bubbleSort.js
+++ Algorithm/Sort Algorithm/Bubble Sort/bubbleSort.js
@@ -1,5 +1,6 @@
 
-//  bubble sort's dummy version:
+/* bubble sort's dummy version: */
+/*
 function sort(arr){
   for(let i=0; i<arr.length; i++){ 
     for(let j=0; j<arr.length; j++){
@@ -13,5 +14,25 @@ function sort(arr){
   }
   return arr;
 }
+*/
+
+
+/* bubble sort's effient version: 注意循环条件*/
+function sort(arr){
+  for(let i = arr.length; i > 0; i--){ 
+    for(let j = 0; j < i-1; j++){
+      if(arr[j] > arr[j+1]){ 
+        // swap :
+        let tmp = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = tmp;
+      }
+    }
+  }
+  return arr;
+}
+
+
 
 sort([100,23,6,0,99,45])  // [0,6,23,45,99, 100]
+23 6 0 99 45 100
