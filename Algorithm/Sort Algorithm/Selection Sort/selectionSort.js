c00a2102bdc9a812c35209fc232e95c5f1c7ba71
Sun flower <MLIU2@LTU.EDU>
2021-03-10 21:30:24 -0600

    Update selectionSort.js

--- Algorithm/Sort Algorithm/Selection Sort/selectionSort.js
+++ Algorithm/Sort Algorithm/Selection Sort/selectionSort.js
@@ -1,4 +1,28 @@
 /* 
   Selection Sort:
-
 */
+
+function selectionSort(arr){
+
+  for(let i=0; i<arr.length; i++){
+    let minIndex = i;
+
+    for(let j=i+1; j <arr.length; j++){
+      if(arr[j]<arr[minIndex])   
+        minIndex = j ;
+    }
+
+    if(i !== minIndex){
+      let tmp = arr[minIndex];
+      arr[minIndex] = arr[i];
+      arr[i] = tmp;
+    }
+
+  }
+
+  return arr;
+}
+
+
+selectionSort([19,5,25,3,10])
+
