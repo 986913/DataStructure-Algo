1aca6b57c9345802c859f73f707f22e4f19ec2f3
Sun flower <MLIU2@LTU.EDU>
2021-03-14 16:00:57 -0500

    quick sort prepare

--- Algorithm/Sort Algorithm/Quick Sort/quickSort.js
+++ Algorithm/Sort Algorithm/Quick Sort/quickSort.js
@@ -1,1 +1,16 @@
-// https://www.youtube.com/watch?v=5nXrEBhBFpU&ab_channel=%E6%84%9B%E8%93%81AiZhen
+/* 
+  quick sort思路: https://www.youtube.com/watch?v=5nXrEBhBFpU&ab_channel=%E6%84%9B%E8%93%81AiZhen
+  quick sort视频： https://www.youtube.com/watch?v=-5F4RafHEvs&ab_channel=%E5%B0%9A%E7%A1%85%E8%B0%B7IT%E5%9F%B9%E8%AE%AD%E5%AD%A6%E6%A0%A1
+*/
+
+
+
+/* privot helper :
+1. privor helper function responsible arranging elments in an anrry on either side of a privot
+2. given an array, this helper function should designate an element as the privot
+3. it should then rearrange elements in the array so that all values less than the privot are moved to the left of the pivot,
+    and all values greater than the pivot are moved to the right of the pivot
+4. the order of elements on either side of the pivot dosen't matter
+5. the helper should do this in place, it should not create a new array
+6. when complete, the helper should return the index of the pivot
+*/
