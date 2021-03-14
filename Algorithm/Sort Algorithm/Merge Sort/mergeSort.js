a7b34332179e4746db71d4553e0479111d15f497
Sun flower <MLIU2@LTU.EDU>
2021-03-13 19:32:32 -0600

    merge sort update

--- Algorithm/Sort Algorithm/Merge Sort/mergeSort.js
+++ Algorithm/Sort Algorithm/Merge Sort/mergeSort.js
@@ -5,6 +5,13 @@
   原理： 假设初始序列有n个记录，就可以看成是n个有序的子序列，每个子序列的长度为1，然后两两merge,
         得到n/2个长度为2或1的有序子序列，再两两merge, 如此重复， 直到得到一个长度为n的有序序列为止
         这种方法被称为 2路merge sort
+
+  Time complexity:
+        best:  O(n log n)
+    average:   O(n log n)
+      worst:   O(n log n)
+
+  Space complexity:  O(n)
 */
 
 // Merges two already sorted arrays
@@ -32,4 +39,16 @@ function merge(arr1, arr2) {
   }
   return results;
 }
-console.log(merge([100, 200], [1, 2, 3, 5, 6]));
+
+
+function mergeSort(arr){
+  if(arr.length <= 1) return arr;
+  
+  let mid = Math.floor(arr.length/2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+
+  return merge(left, right)  // helper function
+}
+
+console.log(mergeSort([8,3,99,12,1,100,74]))
