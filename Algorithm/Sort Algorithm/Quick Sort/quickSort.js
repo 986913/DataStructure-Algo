3a536d42a6b0720b8590319d66b68dcdd503131a
Sun flower <MLIU2@LTU.EDU>
2021-03-15 22:50:47 -0500

    Update quickSort.js

--- Algorithm/Sort Algorithm/Quick Sort/quickSort.js
+++ Algorithm/Sort Algorithm/Quick Sort/quickSort.js
@@ -45,5 +45,18 @@ function privot(arr, start = 0, end = arr.length + 1) {
   swap(arr, start, swapIndex);
   return swapIndex;
 }
+// console.log(privot([5,2,1,8,4,7,6,3]))  //4
 
-console.log(privot([5,2,1,8,4,7,6,3]))  //4
+function quickSort(arr, left = 0, right = arr.length -1){
+  if(left < right){
+      let pivotIndex = pivot(arr, left, right) //3
+      //left
+      quickSort(arr,left,pivotIndex-1);
+      //right
+      quickSort(arr,pivotIndex+1,right);
+    }
+  return arr;
+} 
+      
+quickSort([100,-3,2,4,6,9,1,2,5,3,23])
+
