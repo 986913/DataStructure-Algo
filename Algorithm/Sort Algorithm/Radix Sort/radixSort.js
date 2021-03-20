baae396a4ac5e4486a7618f351e40df2c9780ff5
Sun flower <MLIU2@LTU.EDU>
2021-03-20 18:18:44 -0500

    Update radixSort.js

--- Algorithm/Sort Algorithm/Radix Sort/radixSort.js
+++ Algorithm/Sort Algorithm/Radix Sort/radixSort.js
@@ -1,10 +1,7 @@
 /* Radix sorting:   */
 
-/* helper function:
-
-in order to implement radix sort, it's helpful to build helper function first,
+/* helper function1 :
 getDigit(num, place)- returns the digit in num at the given place value
-
 eg:
     getDigit(12345, 0)    //5
     getDigit(12345, 1)    //4
@@ -13,3 +10,4 @@ eg:
     getDigit(12345, 4)    //1
     getDigit(12345, 5)    //0
 */
+const getDigit = (num, place) => Math.floor(Math.abs(num)/Math.pow(10, place))%10
