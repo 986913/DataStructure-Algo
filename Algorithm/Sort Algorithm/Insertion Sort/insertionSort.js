6d51043868bebe579f450502a9ff368b016649d0
Sun flower <MLIU2@LTU.EDU>
2021-03-26 00:27:04 -0500

    Update insertionSort.js

--- Algorithm/Sort Algorithm/Insertion Sort/insertionSort.js
+++ Algorithm/Sort Algorithm/Insertion Sort/insertionSort.js
@@ -6,16 +6,20 @@
     将它插入到有序表中的适当位置， 使之成为新的有序表
 */
 
-function insertionSort(arr) {
+const insertionSort = (arr) => {
   for (let i = 1; i < arr.length; i++) {
-    let currentVal = arr[i];
 
-    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
-      arr[j + 1] = arr[j];
+    let currentValue = arr[i];
+
+    while (i > 0 && arr[i - 1] > currentValue) {
+      arr[i] = arr[i - 1];
+      i--;
     }
-
-    arr[j + 1] = currentVal;
+    arr[i] = currentValue;
+    
   }
+
   return arr;
-}
+};
+
 insertionSort([-2, 100, 34, 1, 0, 4, 88, 67]);
