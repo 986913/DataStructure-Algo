228ef9d0469878ba7fe5f100627fdbe339ac1da0
Sun flower <MLIU2@LTU.EDU>
2021-03-29 23:52:28 -0500

    Create 1086.js

---
+++ Leetcode/1086.js
@@ -0,0 +1,11 @@
+/* Goal:
+  get average of top 5 scores of each student & arrange in order of each id
+
+  eg: 
+    Input: [[1,91],[1,92],[2,93],[2,97],[1,60],[2,77],[1,65],[1,87],[1,100],[2,100],[2,76]]
+    Output: [[1,87],[2,88]]
+      Explanation: 
+        The average of the student with id = 1 is 87.
+        The average of the student with id = 2 is 88.6. But with integer division their average converts to 88.
+*/
+const highFive = () => {};
