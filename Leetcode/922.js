4a2096444111b0129c48836648055c58265e5c97
Sun flower <MLIU2@LTU.EDU>
2021-03-31 20:11:06 -0500

    Update 922.js

--- Leetcode/922.js
+++ Leetcode/922.js
@@ -20,9 +20,27 @@ var sortArrayByParityII = function (nums) {
 
   return result;
 };
-
-
 console.log(sortArrayByParityII([3,1,4,2]))  //[2,1,4,3]
 
 
 // 2 pointer solution
+const swap = (arr, i, j) => {
+  let tmp = arr[i];
+  arr[i]=arr[j];
+  arr[j]=tmp;
+}
+
+var sortArrayByParityII = function(nums) {
+  let i = 0;
+  let j = 1;
+  let len = nums.length;
+  
+  while(i<len && j<len){
+      while(i<len && nums[i]%2===0) i+=2;
+      while(j<len && nums[j]%2===1) j+=2;
+      if(i<len && j<len) swap(nums,i,j)
+  }
+  
+  return nums;
+};
+console.log(sortArrayByParityII([3,1,4,2]))  //[2,1,4,3]
