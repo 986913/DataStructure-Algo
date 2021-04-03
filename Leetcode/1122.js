adfdf81ca7ea2dd271e3a8b8e7f5fef3dadad070
Sun flower <MLIU2@LTU.EDU>
2021-04-03 00:08:13 -0500

    Update 1122.js

--- Leetcode/1122.js
+++ Leetcode/1122.js
@@ -3,4 +3,24 @@
  * @param {number[]} arr2
  * @return {number[]}
  */
-var relativeSortArray = function (arr1, arr2) {};
+
+// force brute...
+var relativeSortArray = function (arr1, arr2) {
+  let results = [];
+
+  for (let i = 0; i < arr2.length; i++) {
+    for (let j = 0; j < arr1.length; j++) {
+      if (arr2[i] === arr1[j]) {
+        results.push(arr1[j]);
+        arr1[j] = "str";
+      }
+    }
+  }
+  const dontAppear = arr1
+    .filter((item) => typeof item === "number")
+    .sort((a, b) => a - b);
+
+  return results.concat(dontAppear);
+};
+
+console.log(relativeSortArray([28,6,22,8,44,17],[22,28,8,6])) //[22,28,8,6,17,44]
