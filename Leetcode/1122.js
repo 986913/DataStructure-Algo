4da5aa4320bee334fa5918626ad2c9c59ac80fe8
Sun flower <MLIU2@LTU.EDU>
2021-04-03 18:28:25 -0500

    Update 1122.js

--- Leetcode/1122.js
+++ Leetcode/1122.js
@@ -16,11 +16,41 @@ var relativeSortArray = function (arr1, arr2) {
     }
   }
   const difference = arr1.filter(item=>!results.includes(item)).sort((a,b)=>a-b);
-  
+
   return results.concat(difference);
 };
-
 console.log(relativeSortArray([28,6,22,8,44,17],[22,28,8,6])) //[22,28,8,6,17,44]
 
+/* 解法2： Radix sort */
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number[]}
+ */
+ var relativeSortArray = function(arr1, arr2) {
+  let arr = new Int8Array(1001);
+  let result = [];
+  
+  for(let i=0; i<arr1.length; i++){
+    arr[arr1[i]]++
+  }
+
+  for(let num of arr2){
+    while(arr[num]!==0){
+      result.push(num);
+      arr[num]--;
+    }
+  }
+
+  for(let i=0; i<arr.length; i++){
+    while(arr[i]!==0){
+      result.push(i);
+      arr[i]--
+    }
+  }
+  
+  return result
+};
+console.log(relativeSortArray([28,6,22,8,44,17],[22,28,8,6])) //[22,28,8,6,17,44]
 
 
