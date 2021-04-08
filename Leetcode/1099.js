ca96406e845c42be1452eab396dd1005cc5db2b8
Sun flower <MLIU2@LTU.EDU>
2021-04-07 21:51:58 -0500

    Update 1099.js

--- Leetcode/1099.js
+++ Leetcode/1099.js
@@ -5,5 +5,25 @@
 
   eg:
     twoSumLessThanK([34,23,1,24,75,33,54,8], 60)  // 58  解释: we can use 34 and 24 to sum 58 which is less than 60
-      twoSumLessThanK([10,20,30], 15)             // -1  解释: it's not possible to get a pair sum less than 15
+    twoSumLessThanK([10,20,30], 15)               // -1  解释: it's not possible to get a pair sum less than 15
  */
+
+  const twoSumLessThanK = (ARR, K) => {
+    let max = -Infinity;
+    let sorted = ARR.sort((a,b)=>a-b);
+    let left =0; let right =ARR.length-1;
+
+    while(left<right){
+      if(sorted[left]+sorted[right]>K){
+        right--;
+      }else{
+        left++;
+        max = Math.max(max, sorted[left]+sorted[right])
+      }
+    }
+
+    return max;
+  }
+
+  console.log(twoSumLessThanK([34,23,1,24,75,33,54,8], 60))  //58
+  console.log(twoSumLessThanK([10,20,30], 15))    //-1
