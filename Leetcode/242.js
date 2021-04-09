ee5ec19b02b03e3365826678d1158a20dc77c668
Sun flower <MLIU2@LTU.EDU>
2021-04-08 22:11:14 -0500

    Update 242.js

--- Leetcode/242.js
+++ Leetcode/242.js
@@ -10,3 +10,24 @@ var isAnagram = function(s, t) {
   const sortedT = t.split('').sort().join('');
   return sortedS===sortedT
 };
+
+
+// Solution2: use map
+var isAnagram = function(s, t) {
+  const map = new Map();
+
+  for(let i=0; i<t.length; i++){
+    map.set(t[i], map.get(t[i])+1||1)
+  }
+
+  for(let i=0; i<s.length; i++){
+    if(!map.has(s[i])) return false
+    map.set(s[i], map.get(s[i])-1)
+  }
+  
+  for(let [key, val] of map){
+    if(val!==0) return false
+  }
+
+  return true;
+};
