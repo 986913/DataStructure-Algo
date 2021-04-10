b55efd2c9473bb8975313432b6625a228592ede2
Sun flower <MLIU2@LTU.EDU>
2021-04-09 22:51:16 -0500

    Update 252.js

--- Leetcode/252.js
+++ Leetcode/252.js
@@ -10,3 +10,16 @@ Output: false
 Input: [[7,10],[2,4]]
 Output: true
 */
+
+const canAttendAll = (arr) => {
+  arr.sort((a,b)=>a[0]-b[0]);
+  console.log(arr)
+  for(let i=0; i<arr.length-1; i++){
+    if(arr[i][1]>arr[i+1][1])
+      return false
+  }
+  return true
+}
+
+console.log(canAttendAll([[0,30],[5,10],[15,20]])) // false
+console.log(canAttendAll([[7,10],[2,4]]))
