1bef22d1844c80d2f6431545d7ed0ac68508e93a
Sun flower <MLIU2@LTU.EDU>
2021-04-11 15:38:32 -0500

    Update 350.js

--- Leetcode/350.js
+++ Leetcode/350.js
@@ -6,7 +6,7 @@
 
 
 var intersect = function(nums1, nums2) {
-  
+
   nums1.sort((a,b)=>a-b);
   nums2.sort((a,b)=>a-b);
   let i=0;
@@ -27,3 +27,6 @@ var intersect = function(nums1, nums2) {
 
   return result;
 };
+
+console.log(intersect([1,2,2,1], [2,2]));  //[2,2]
+console.log(intersect([4,9,5], [9,4,9,8,4]))  //[4,9]    //[9,4] is also accepted.
