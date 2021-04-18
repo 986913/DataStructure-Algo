660020402bae880cd050705b64540e82ad55a916
Sun flower <MLIU2@LTU.EDU>
2021-04-17 21:44:31 -0500

    Update 3.js

--- Leetcode/3.js
+++ Leetcode/3.js
@@ -3,6 +3,24 @@
  * @return {number}
  */
 
+// 暴力法：
+var lengthOfLongestSubstring = function(s) {
+  let maxSize = 0;
+  
+  for(let i=0 ; i<s.length; i++){
+    let set = new Set();
+    for(let j=i; j<s.length; j++){
+      if(set.has(s[j])) break;
+      set.add(s[j])
+    } 
+    maxSize = Math.max(maxSize, set.size)
+  }
+  
+  return maxSize
+};
+
+
+// Slding window
 var lengthOfLongestSubstring = function(s) {
   let maxLen = 0;
   let arrWindow = [];
