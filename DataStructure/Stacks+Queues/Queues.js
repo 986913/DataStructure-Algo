a33b5ae623c7171121421801f41e3e80b946783c
Sun flower <MLIU2@LTU.EDU>
2021-04-26 22:30:40 -0500

    create stacks and queues files

---
+++ DataStructure/Stacks+Queues/Queues.js
@@ -0,0 +1,9 @@
+/*
+an array-like data structure whose elements follow the FIFO rule: First In, First Out
+- the following are a queue's standard operations and their time complexities:
+    - Enqueuing an element into the queue —> O(1)
+    - Dequeuing an element out of the queue ——> O(1)
+    - peeking at the element on the front of the queue —→O(1)
+    - searching for an element in the queue ——→ O(n)
+A stack is typically implemented with a doubly linked list 
+*/
---
+++ DataStructure/Stacks+Queues/Stacks.js
@@ -0,0 +1,8 @@
+/* an array-like data structure whose elements follow the LIFO rule: Last In, First Out
+  the following are a stack's standard operations and their time complexities:
+    - pushing an element onto the stack —> O(1)
+    - popping an element off the stack ——> O(1)
+    - peeking at the element on the top of the stack —→O(1)
+    - searching for an element in the stack ——→ O(n)
+- A stack is typically implemented with a dynamic array  or with a singly linked list
+*/
