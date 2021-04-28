c78a21fd7e62df2cfe4e398fd55072e484cccf4d
Sun flower <MLIU2@LTU.EDU>
2021-04-28 16:00:40 -0500

    Create 1021.js

---
+++ Leetcode/1021.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+
+// brute force:
+var removeOuterParentheses = function (S) {
+  let decomposition = [];     // use to save decomposition 
+  let chars = "";             // each item of decomposition
+  let leftAmount = 0;
+  let rightAmount = 0;
+  let result = "";
+
+  for (let i = 0; i < S.length; i++) {
+    if (!chars || leftAmount !== rightAmount) {
+      if (S[i] === "(") leftAmount += 1;
+      else rightAmount += 1;
+
+      chars += S[i];
+    }
+    if (leftAmount === rightAmount) {
+      decomposition.push(chars);
+      chars = "";
+    }
+  }
+
+  decomposition.forEach((char) => {
+    const newchar = char.substring(1, char.length - 1);
+    result += newchar;
+  });
+
+  return result;
+};
