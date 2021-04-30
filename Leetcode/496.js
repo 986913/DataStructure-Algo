aae2b27166d0c58dbccbb8cf9319789c1f8f290d
Sun flower <MLIU2@LTU.EDU>
2021-04-30 15:03:53 -0500

    Update 496.js

--- Leetcode/496.js
+++ Leetcode/496.js
@@ -26,3 +26,46 @@ var nextGreaterElement = function (nums1, nums2) {
 
   return result;
 };
+
+
+// solution2: 
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+
+// https://leetcode-cn.com/problems/next-greater-element-i/solution/dong-hua-yan-shi-dan-diao-zhan-496xia-yi-ql65/
+
+var nextGreaterElement = function(nums1, nums2) {
+  let lookup = {};
+  let stack = [];
+  let result = [];
+  
+  for(let i=0; i<nums2.length; i++){
+    // while循环表示当前元素是栈中所有已存元素的下一个更大元素
+    while(stack && nums2[i]> stack[stack.length-1]){
+      const key = stack.pop();
+      const value = nums2[i];   
+      
+      lookup[key]=value;
+    }
+  
+    stack.push(nums2[i])  
+  }
+  // console.log(stack, lookup)
+  // 栈中剩余的元素是没有下一个更大元素的元素
+  while(stack.length>0){
+    const key = stack.pop();
+    lookup[key]=-1
+  }
+  // console.log(stack, lookup)
+  
+  for(let i=0; i<nums1.length; i++){
+    result[i] = lookup[nums1[i]]
+  }
+  
+  return result
+};
+
+
