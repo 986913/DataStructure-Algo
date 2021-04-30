fe22b2a31d1dcfe7bd6a95ca1b214ddc5ea02638
Sun flower <MLIU2@LTU.EDU>
2021-04-30 16:08:24 -0500

    Update 20.js

--- Leetcode/20.js
+++ Leetcode/20.js
@@ -33,6 +33,10 @@ var isValid = function (s) {
       stack.push(s[i])
     }
   }
-  
+
   return stack.length === 0;
 };
+
+
+console.log(isValid('()[]')) //true
+console.log(isValid('([{])}')) //false
