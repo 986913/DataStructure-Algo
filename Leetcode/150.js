33c761d478d20dd50e4047e2389664510f79f1ae
Sun flower <MLIU2@LTU.EDU>
2021-05-03 00:53:05 -0500

    Create 150.js

---
+++ Leetcode/150.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+  var stack = [];
+  var right, left;
+
+  for (let i = 0; i < tokens.length; i++) {
+    switch (tokens[i]) {
+      case "+":
+        stack.push(stack.pop() + stack.pop());
+        break;
+      case "-":
+        right = stack.pop();
+        left = stack.pop();
+        stack.push(left - right); //要左边减右边
+        break;
+      case "*":
+        stack.push(stack.pop() * stack.pop());
+        break;
+      case "/":
+        right = stack.pop();
+        left = stack.pop();
+        stack.push((left / right) | 0); //要左边除右边
+        break;
+      default:
+        stack.push(parseInt(tokens[i]));
+    }
+  }
+  return stack[0];
+};
