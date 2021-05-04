cb49563091413a844714f063fd3b4499c8aaa5b3
Sun flower <MLIU2@LTU.EDU>
2021-05-03 23:31:51 -0500

    Create 155.js

---
+++ Leetcode/155.js
@@ -0,0 +1,48 @@
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function () {
+  this.stack = [];
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+  this.stack.push(val);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  let min = Infinity;
+  this.stack.forEach((ele) => {
+    if (ele < min) min = ele;
+  });
+  return min;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
