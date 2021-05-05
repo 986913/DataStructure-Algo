dc0f94f7861a6f416faa0e664bdf226f0cbc81de
Sun flower <MLIU2@LTU.EDU>
2021-05-05 15:48:19 -0500

    Update BinarySeachTrees.js

--- DataStructure/Tree/BinarySeachTrees.js
+++ DataStructure/Tree/BinarySeachTrees.js
@@ -1,4 +1,3 @@
-
 class Node {
   constructor(value) {
     this.value = value;
@@ -6,6 +5,7 @@ class Node {
     this.right = null;
   }
 }
+
 class BinarySearchTree {
   constructor() {
     this.root = null;
@@ -13,13 +13,11 @@ class BinarySearchTree {
 
   insert(val) {
     const newNode = new Node(val);
-
-    if (this.root === null) {
+    if (!this.root) {
       this.root = newNode;
       return this;
     } else {
       let current = this.root;
-
       while (true) {
         if (val === current.value) return undefined;
 
@@ -29,7 +27,8 @@ class BinarySearchTree {
             return this;
           }
           current = current.left;
-        } else { // val > current.value
+        } else {
+          // val > current.value
           if (current.right === null) {
             current.right = newNode;
             return this;
@@ -40,7 +39,23 @@ class BinarySearchTree {
     }
   }
 
-  find(val) {}
+  contains(val) {
+    if (!this.root) return false;
+    
+    let current = this.root;
+    while (current) {
+      if (val < current.value) {
+        current = current.left;
+      } else if (val > current.value) {
+        current = current.right;
+      } else {
+        // val === current.value
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
 
 const tree = new BinarySearchTree();
@@ -51,5 +66,5 @@ tree.insert(11);
 tree.insert(2);
 tree.insert(16);
 tree.insert(7);
-
+console.log(tree.contains(8));
 console.log(tree);
