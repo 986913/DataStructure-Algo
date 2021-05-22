ec118c923a9f70bdb68df139c32478e31c5b1bab
Sun flower <MLIU2@LTU.EDU>
2021-05-22 11:28:10 -0500

    Create 5.js

---
+++ Leetcode/5.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+/* Soultion1:  自己写的暴力法。 时间耗时太长 */
+var longestPalindrome = function (s) {
+  const isPalindromic = (str) => str === str.split("").reverse().join("");
+  
+  let palindromicArr = []; //holds all palindromic combo
+
+  for (let i = 0; i < s.length; i++) {
+    for (let j = 0; j <= s.length; j++) {
+      let subStr = s.slice(i, j);
+      if (isPalindromic(subStr) && subStr !== "") {
+        palindromicArr.push(subStr);
+      }
+    }
+  }
+
+  // return most longest one of palindromicArr
+  let maxLen = 0;
+  let result = "";
+  palindromicArr.forEach((item) => {
+    if (item.length > maxLen) {
+      maxLen = item.length;
+      result = item;
+    }
+  });
+  return result;
+};
