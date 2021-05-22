4202a5fe453d1f2750d716a15f41816a1279e159
Sun flower <MLIU2@LTU.EDU>
2021-05-22 11:28:30 -0500

    Update 5.js

--- Leetcode/5.js
+++ Leetcode/5.js
@@ -3,10 +3,10 @@
  * @return {string}
  */
 
-/* Soultion1:  自己写的暴力法。 时间耗时太长 */
+/* Soultion1:  自己写的暴力法。 时间耗时太长 不推荐用 */
 var longestPalindrome = function (s) {
   const isPalindromic = (str) => str === str.split("").reverse().join("");
-  
+
   let palindromicArr = []; //holds all palindromic combo
 
   for (let i = 0; i < s.length; i++) {
