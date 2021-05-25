b97d810c5baf0fab0bf0b8c2c127171dcc370b7a
Sun flower <MLIU2@LTU.EDU>
2021-05-24 21:57:18 -0500

    Update 5.js

--- Leetcode/5.js
+++ Leetcode/5.js
@@ -31,3 +31,54 @@ var longestPalindrome = function (s) {
 };
 
 /* Soulution2: DP */
+/* 
+  维护一个dp维数组,  dp[i][j]的i,j代表string的index, dp[i][j]存的是true/false,代表是不是palindrome
+  https://leetcode.com/problems/longest-palindromic-substring/discuss/1167311/JavaScript-solution-Dynamic-Programming-Time-O(n2)-Space-O(n2)-with-Explanation 
+*/
+
+var longestPalindrome = function (s) {
+  let palindrome;
+
+  // 2D Array
+  dp = Array(s.length)
+    .fill(0)
+    .map(() => Array(s.length).fill(false));
+
+  // Handle One Character.
+  for (let row = 0; row < s.length; row++) {
+    for (let column = row; column < s.length; column++) {
+      // console.log(`{s[row]})
+      if (row === column) {
+        dp[row][column] = true;
+        palindrome = s[row];
+      }
+    }
+  }
+
+  // Handle Two Characters
+  for (let r = 0; r < s.length - 1; r++) {
+    for (let c = r + 1; c > r; c--) {
+      //    console.log(`${s[r]}${s[c]}`)
+      if (s[r] === s[c]) {
+        dp[r][c] = true;
+        palindrome = s.substring(r, c + 1);
+      }
+    }
+  }
+
+  // Handle Three or more Characteres. This is the real part of the algorithm.
+  for (let c = 2; c < s.length; c++) {
+    for (let r = 0; r + c < s.length; r++) {
+      let wordLength = r + c;
+      // console.log(`${s.substring(r, wordLength+1)}`)
+      if (s[r] === s[wordLength] && dp[r + 1][wordLength - 1]) {
+        dp[r][wordLength] = true;
+        let newPalindrome = s.substring(r, wordLength + 1);
+        if (newPalindrome.length > palindrome.length) {
+          palindrome = newPalindrome;
+        }
+      }
+    }
+  }
+  return palindrome;
+};
