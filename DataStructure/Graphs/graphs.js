86ccac08591666f5419f53b5e390f7ea16c7597a
Sun flower <MLIU2@LTU.EDU>
2021-07-09 17:15:36 -0500

    Update graphs.js

--- DataStructure/Graphs/graphs.js
+++ DataStructure/Graphs/graphs.js
@@ -5,10 +5,28 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
+    } else if (!this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1] = [vertex2];
+      this.adjacencyList[vertex2].push(vertex1);
+    } else if (this.adjacencyList[vertex1] && !this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2] = [vertex1];
+    } else {
+      this.adjacencyList[vertex1] = [vertex2];
+      this.adjacencyList[vertex2] = [vertex1];
+    }
+  }
 }
 
 let g = new Graph();
 g.addVertex("changan");
-g.addVertex("beijin");
-
+g.addVertex("beijing");
+g.addEdge("changan", "beijing");
+g.addEdge("xian", "detroit");
+g.addEdge("xian", "beijing");
+g.addEdge("xian", "changan");
 console.log(g);
