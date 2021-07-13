d33f051f55b79eea1ff0c4fcf35ceca01dabe057
Sun flower <MLIU2@LTU.EDU>
2021-07-13 14:28:59 -0500

    Update graphs.js

--- DataStructure/Graphs/graphs.js
+++ DataStructure/Graphs/graphs.js
@@ -2,11 +2,9 @@ class Graph {
   constructor() {
     this.adjacencyList = {};
   }
-  
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
-
   addEdge(vertex1, vertex2) {
     if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
       this.adjacencyList[vertex1].push(vertex2);
@@ -22,6 +20,15 @@ class Graph {
       this.adjacencyList[vertex2] = [vertex1];
     }
   }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+  }
 }
 
 let g = new Graph();
@@ -31,4 +38,5 @@ g.addEdge("changan", "beijing");
 g.addEdge("xian", "detroit");
 g.addEdge("xian", "beijing");
 g.addEdge("xian", "changan");
+g.removeEdge("xian", "changan");
 console.log(g);
