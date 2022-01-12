f786f7a1302e69b5a77a18446364673240fb2543
Sun flower <MLIU2@LTU.EDU>
2022-01-12 00:20:56 -0500

    Update 697.js

--- Leetcode/697.js
+++ Leetcode/697.js
@@ -1,3 +1,28 @@
+/**
+ * @param {number[]} nums: [2,1,1,2,1,3,3,3,1,3,1,3,2]
+ * @return {number}: 7
+ */
+var findShortestSubArray = function (nums) {
+  let frequencyMap = new Map();
+  let degree = 0;
+
+  let firstShowMap = new Map();
+  let minlen = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!firstShowMap.has(nums[i])) firstShowMap.set(nums[i], i);
+    frequencyMap.set(nums[i], (frequencyMap.get(nums[i]) || 0) + 1);
+
+    if (frequencyMap.get(nums[i]) > degree) {
+      degree = frequencyMap.get(nums[i]);
+      minlen = i - firstShowMap.get(nums[i]) + 1;
+    } else if (frequencyMap.get(nums[i]) === degree) {
+      minlen = Math.min(minlen, i - firstShowMap.get(nums[i]) + 1);
+    }
+  }
+  return minlen;
+};
+
 var findShortestSubArray = function (nums) {
   const n = nums.length;
   const map = new Map();
