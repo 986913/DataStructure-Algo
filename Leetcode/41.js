a75dc68b61e9571bfd43bec2f054c28bad08cb90
Sun flower <MLIU2@LTU.EDU>
2022-01-24 22:14:45 -0500

    Create 41.js

---
+++ Leetcode/41.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function (nums) {
+  let missing = 0;
+
+  for (let i = 1; i <= nums.length; i++) {
+    let count = 0;
+
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[j] === i) count++;
+    }
+
+    if (count === 0) missing = i;
+    if (missing > 0) break;
+  }
+
+  if (missing === 0) return nums.length + 1;
+  return missing;
+};
