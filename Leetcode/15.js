50239716484fa4089d844073eaf64da23598eb70
Sun flower <MLIU2@LTU.EDU>
2022-02-05 14:31:46 -0500

    Update 15.js

--- Leetcode/15.js
+++ Leetcode/15.js
@@ -16,3 +16,40 @@ const threeSum = function (nums) {
   }
   return setResult.map((item) => item.split(' '));
 };
+
+//转换为2sum
+v;
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+  let result = [];
+  nums.sort((a, b) => a - b);
+
+  if (nums.length < 3) return result;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue; //skip dupicated
+
+    let j = i + 1;
+    let k = nums.length - 1;
+    while (j < k) {
+      if (nums[i] + nums[j] + nums[k] === 0) {
+        result.push([nums[i], nums[j], nums[k]]);
+        j++;
+        k--;
+        while (j < k && nums[j] === nums[j - 1]) j++; //skip dupicated
+        while (j < k && nums[k] === nums[k + 1]) k--; //skip dupicated
+      } else if (nums[i] + nums[j] + nums[k] < 0) {
+        j++;
+        while (j < k && nums[j] === nums[j - 1]) j++;
+      } else {
+        k--;
+        while (j < k && nums[k] === nums[k + 1]) k--;
+      }
+    }
+  }
+
+  return result;
+};
