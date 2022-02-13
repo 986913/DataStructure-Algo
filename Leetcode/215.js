8782fc496e00215e8661b8663b444d0d45e796aa
Sun flower <MLIU2@LTU.EDU>
2022-02-13 15:40:18 -0500

    Update 215.js

--- Leetcode/215.js
+++ Leetcode/215.js
@@ -18,3 +18,36 @@ solution2: quick select:
 
   这个方法的理论依据是 partition 得到的点的下标就是最终排序之后的下标，根据这个下 标，我们可以判断第 K 大的数在哪里
  */
+const findKthLargest = function (nums, k) {
+  k = nums.length - k;
+  return quickselect(0, nums.length - 1);
+
+  function quickselect(left, right) {
+    if (left === right) return nums[left];
+    let randomIdx = left + Math.floor(Math.random() * (right - left));
+    let pivotIdx = partition(left, right, randomIdx);
+    if (k < pivotIdx) return quickselect(left, pivotIdx);
+    else if (k > pivotIdx) return quickselect(pivotIdx + 1, right);
+    return nums[k];
+  }
+
+  function partition(left, right, randomIdx) {
+    const pivot = nums[randomIdx];
+    swap(randomIdx, right);
+    let pointer = left;
+    for (let idx = left; idx <= right; idx++) {
+      if (nums[idx] < pivot) {
+        swap(pointer, idx);
+        pointer++;
+      }
+    }
+    swap(pointer, right);
+    return pointer;
+  }
+
+  function swap(idx1, idx2) {
+    let temp = nums[idx1];
+    nums[idx1] = nums[idx2];
+    nums[idx2] = temp;
+  }
+};
