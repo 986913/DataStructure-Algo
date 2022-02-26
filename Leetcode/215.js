503261dabfebd534be3299dc547af122da6fd2f5
Sun flower <MLIU2@LTU.EDU>
2022-02-26 15:10:48 -0500

    Update 215.js

--- Leetcode/215.js
+++ Leetcode/215.js
@@ -4,9 +4,23 @@
  */
 
 const findKthLargest = function (nums, k) {
+  /*这是建立Min-Heap的解法， 推荐！ */
   let minHeap = new MinHeap(k);
   minHeap.build(nums);
   return minHeap.peek();
+
+  /*这是用Max-Heap的解法， 不推荐。。 */
+  /*
+  let maxHeap = new MaxHeap(nums.length);
+  maxHeap.build(nums);
+
+  let result = 0;
+  while (k > 0) {
+    result = maxHeap.removeRoot();
+    k--;
+  }
+  return result;
+  */
 };
 
 class Heap {
