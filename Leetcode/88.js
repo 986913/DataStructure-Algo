7c71bd00ce2930053dda81f4152cb00e1ba62b5f
Sun flower <MLIU2@LTU.EDU>
2022-03-05 21:19:39 -0500

    Update 88.js

--- Leetcode/88.js
+++ Leetcode/88.js
@@ -14,3 +14,24 @@ var merge = function (nums1, m, nums2, n) {
 
   nums1.sort((a, b) => a - b);
 };
+
+//Approach 2:  2 points
+var merge = function (nums1, m, nums2, n) {
+  let p1 = m - 1; //tracking nums1
+  let p2 = n - 1; //tracking nums2
+  let p = m + n - 1; //modify nums1
+
+  while (p >= 0) {
+    if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+      nums1[p] = nums1[p1];
+      p1--;
+    } else {
+      if (p2 >= 0) {
+        nums1[p] = nums2[p2];
+        p2--;
+      }
+    }
+
+    p--;
+  }
+};
