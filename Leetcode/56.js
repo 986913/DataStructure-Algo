1a237657316a91d52bdbaaddff5875d81a79b4db
Sun flower <MLIU2@LTU.EDU>
2022-03-12 15:13:46 -0500

    Create 56.js

---
+++ Leetcode/56.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+
+var merge = function (intervals) {
+  let result = [];
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  intervals.forEach((item) => {
+    if (result.length === 0) {
+      result.push(item);
+    } else {
+      let last = result[result.length - 1];
+
+      if (item[0] > last[1]) {
+        result.push(item);
+      }
+
+      if (item[0] <= last[1]) {
+        last[0] = Math.min(last[0], item[0]);
+        last[1] = Math.max(last[1], item[1]);
+      }
+    }
+  });
+
+  return result;
+};
