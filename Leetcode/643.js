802117a8939cfb1fc0e3ccd2d22063e1ad0415e1
Sun flower <MLIU2@LTU.EDU>
2022-03-19 15:12:59 -0400

    Update 643.js

--- Leetcode/643.js
+++ Leetcode/643.js
@@ -14,3 +14,17 @@ var findMaxAverage = function (nums, k) {
 
   return max;
 };
+
+var findMaxAverage = function (nums, k) {
+  let max = -Infinity;
+
+  for (let i = 0; i < nums.length - k + 1; i++) {
+    let sum = 0;
+    for (let j = i; j < i + k; j++) {
+      sum += nums[j];
+    }
+    max = Math.max(max, sum / k);
+  }
+
+  return max;
+};
