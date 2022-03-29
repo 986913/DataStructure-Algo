ac32303c61ccdb716042ebbac64a60c13d0181ce
Sun flower <MLIU2@LTU.EDU>
2022-03-28 20:50:44 -0400

    Update 3.js

--- Leetcode/3.js
+++ Leetcode/3.js
@@ -4,36 +4,55 @@
  */
 
 // 暴力法：
-var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function (s) {
   let maxSize = 0;
-  
-  for(let i=0 ; i<s.length; i++){
+
+  for (let i = 0; i < s.length; i++) {
     let set = new Set();
-    for(let j=i; j<s.length; j++){
-      if(set.has(s[j])) break;
-      set.add(s[j])
-    } 
-    maxSize = Math.max(maxSize, set.size)
+    for (let j = i; j < s.length; j++) {
+      if (set.has(s[j])) break;
+      set.add(s[j]);
+    }
+    maxSize = Math.max(maxSize, set.size);
   }
-  
-  return maxSize
+
+  return maxSize;
 };
 
-
 // Slding window
-var lengthOfLongestSubstring = function(s) {
-  let maxLen = 0;
-  let arrWindow = [];
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  let windowStartIdx = 0;
+  let longestNonRepeatStrLen = 0;
+  let charIndexMap = new Map();
 
-  for(let i =0; i<s.length; i++){
-    const index = arrWindow.indexOf(s[i]);
-    if(index !== -1){
-      // shrink window
-      arrWindow.splice(0, index+1)
+  // try to extend the range [windowStartIdx, windowEndIdx]
+  for (let windowEndIdx = 0; windowEndIdx < s.length; windowEndIdx++) {
+    const rightChar = s[windowEndIdx];
+    // if the map already contains the 'rightChar', shrink the window from the beginning so that
+    // we have only one occurrence of 'rightChar'
+
+    if (charIndexMap.has(rightChar)) {
+      // this is tricky; in the current window, we will not have any 'rightChar' after its previous index
+      // and if 'windowStart' is already ahead of the last index of 'rightChar', we'll keep 'windowStart'
+      windowStartIdx = Math.max(
+        windowStartIdx,
+        charIndexMap.get(rightChar) + 1
+      );
     }
-    // expand window
-    arrWindow.push(s[i]);
-    maxLen = Math.max(maxLen, arrWindow.length)
+
+    // insert the 'rightChar' into the map
+    charIndexMap.set(rightChar, windowEndIdx);
+
+    // remember the maximum length so far
+    longestNonRepeatStrLen = Math.max(
+      longestNonRepeatStrLen,
+      windowEndIdx - windowStartIdx + 1
+    );
   }
-return maxLen;
+
+  return longestNonRepeatStrLen;
 };
