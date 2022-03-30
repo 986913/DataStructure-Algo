6eccb8e47f5010714d1689b3ebbffa3c571919f5
Sun flower <MLIU2@LTU.EDU>
2022-03-29 22:12:24 -0400

    Update 424.js

--- Leetcode/424.js
+++ Leetcode/424.js
@@ -4,11 +4,17 @@ const length_of_longest_substring = function (str, k) {
   let maxRepeatLetterCount = 0;
   let map = new Map(); // frequency map
 
+  /* Current window size is from windowStart to windowEnd, overall we have a letter which is
+    repeating 'maxRepeatLetterCount' times, this means we can have a window which has one letter
+    repeating 'maxRepeatLetterCount' times and the remaining letters we should replace.
+
+    if the remaining letters are more than 'k', it is the time to shrink the window as we are not allowed to replace more than 'k' letters */
   for (let windowEndIdx = 0; windowEndIdx < str.length; windowEndIdx++) {
     const rightChar = str[windowEndIdx];
-    map.set(rightChar, map.get(rightChar) + 1 || 1);
-    maxRepeatLetterCount = Math.max(maxRepeatLetterCount, map.get(rightChar));
+    map.set(rightChar, map.get(rightChar) + 1 || 1); // create frequency map
+    maxRepeatLetterCount = Math.max(maxRepeatLetterCount, map.get(rightChar)); // update count for repeat letters
 
+    //KEY POINT: window size - repeating letters count = remain letters.  if remain letters > k then shrink the window, as we are not allowed to replace more than 'k' letters
     if (windowEndIdx - windowStartIdx + 1 - maxRepeatLetterCount > k) {
       leftChar = str[windowStartIdx];
       map.set(leftChar, map.get(leftChar) - 1);
