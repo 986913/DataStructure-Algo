44d290620a4d1b6ebd091029e7c77acb8f66c2c6
Mingyue Liu - m0l0dqx <Mingyue.Liu@walmart.com>
2022-03-31 21:24:21 -0400

    26.js

---
+++ Leetcode/26.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+//注意：牵扯到数组里面 “循环”和“删除元素”时候， 最好倒着循环！！！
+ var removeDuplicates = function(nums) {
+  for(let i=nums.length; i>=0; i--){
+      if(nums[i]===nums[i-1]){
+          nums.splice(i, 1);
+          
+      }
+  }
+};
