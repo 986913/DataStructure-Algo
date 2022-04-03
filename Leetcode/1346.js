95b68b692b88696071f01eaf1b5c55d901be6516
Mingyue Liu - m0l0dqx <Mingyue.Liu@walmart.com>
2022-04-03 17:17:56 -0400

    1346.js

--- Leetcode/1346.js
+++ Leetcode/1346.js
@@ -15,3 +15,37 @@
   }
   return false;
 };
+
+
+/**
+ *  Binary search 方法
+ */
+ var checkIfExist = function(arr) {
+  let left = 0;
+  let right = arr.length-1;
+  arr.sort((a,b)=>a-b)
+    
+ for(let i=0; i<arr.length; i++){
+         let result = binarySearch(arr, arr[i]*2);
+         if(result!=-1 && result != i) return true
+ }
+    
+  return false;
+};
+
+const binarySearch = (arr, target) => {
+    let left = 0;
+    let right = arr.length -1;
+    
+    while(left<=right){
+     let mid = Math.floor((left+right)/2);
+          if(arr[mid]===target){
+              return mid
+          }else if(target<arr[mid]){
+                right=mid-1
+          }else{
+                left=mid+1 
+          }
+   }
+  return -1
+}
