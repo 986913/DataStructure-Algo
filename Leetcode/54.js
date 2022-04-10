a959c70243c4d55f1c5a3b0c286c1deb58c560e6
Mingyue Liu - m0l0dqx <Mingyue.Liu@walmart.com>
2022-04-10 11:52:39 -0400

    edit 54.js

--- Leetcode/54.js
+++ Leetcode/54.js
@@ -3,73 +3,53 @@
  * @return {number[]}
  */
 
-var spiralOrder = function (matrix) {
-  if (matrix.length === 0) return [];
-
-  let matrixRow = matrix.length;   //3
-  let matrixColumn = matrix[0].length;  //4
-
-  if (matrixRow === 1 || matrixColumn === 1) return drawBorder(matrix)
-
-  let result = [];
-  while (result.length !== matrixRow * matrixColumn) {
-    // draw border
-    const borders = drawBorder(matrix);
-    // update result;
-    result = result.concat(borders);
-    // then delete border -> create a new matrix
-    matrix = removeNullInsideMatrix(matrix);
-  }
+ var spiralOrder = function(matrix) {
+  const m = matrix.length;    // row of matrix
+  const n = matrix[0].length; // column of matrix
+   
+  let result = [];  // return value
+  
+  /* initial 4 variable for each border's started position */
+  let topBorder = 0; 
+  let bottomBorder = m-1;
+  let leftBorder = 0;
+  let rightBorder = n-1;
+   
+  while(result.length < m*n){
+       
+     //forloop topBorder (left -> right) then update topBorder
+      if(topBorder <= bottomBorder){
+          for(let i=leftBorder; i<=rightBorder; i++){
+           result.push(matrix[topBorder][i])
+         }
+       topBorder++
+     }
+     
+     //forloop rightBorder (top -> down) then update rightBorder
+     if(leftBorder<=rightBorder){
+        for(let i=topBorder; i<=bottomBorder; i++){
+           result.push(matrix[i][rightBorder])
+         }
+        rightBorder--
+     }
+      
+      
+     //forloop bottomBorder (right -> left) then update bottomBorder
+       if(topBorder <= bottomBorder){
+        for(let i=rightBorder; i>=leftBorder; i--){
+           result.push(matrix[bottomBorder][i])
+         }
+        bottomBorder--
+     }
+      
+     //forloop leftBorder  (down -> top) then update leftBorder
+      if(leftBorder<=rightBorder){
+        for(let i=bottomBorder; i>=topBorder; i--){
+           result.push(matrix[i][leftBorder])
+         }
+       leftBorder++
+     }
+   
+  } 
   return result;
 };
-
-const removeNullInsideMatrix = (matrix) => {
-  return matrix.filter((row) => {
-    row.forEach((n, idx) => { if (!n) row.splice(idx, 1) });
-    return row.some((column) => column)
-  })
-}
-
-const drawBorder = matrix => {
-  const borders = [];
-  let row = 0, column = 0;
-  let matrixRow = matrix.length;
-  let matrixColumn = matrix[0].length;
-
-  let indexRecord = [];
-
-  for (let i = 0; i < matrixColumn * matrixRow; i++) {
-
-    indexRecord.push(`${row}-${column}`);   // indexRecord is for memorize row and column history.
-    if (borders.indexOf(`${row}-${column}`) !== -1) break;  //terminate 
-
-
-    if (matrix[row][column] !== null) {
-      borders.push(matrix[row][column]);
-    }
-    matrix[row][column] = null;
-
-    if (row == 0) { // go right
-      if (column === matrixColumn - 1) {   //处理越界
-        row++;
-      }
-      else column++;
-    } else if (column === matrixColumn - 1) {  // go down
-      if (row === matrixRow - 1) {
-        column--;
-      }
-      else row++;
-    } else if (row === matrixRow - 1) {  // go left
-      if (column === 0) {
-        row--;
-      }
-      else column--;
-    } else {                       // go up
-      if (row === 0) {
-        column++;
-      }
-      row--;
-    }
-  }
-  return borders;
-}
