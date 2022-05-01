ceac88cd8e4ea991b4541b32e786802c0a97d13a
Sun flower <MLIU2@LTU.EDU>
2022-05-01 15:31:19 -0400

    581 2nd solution

--- Leetcode/581.js
+++ Leetcode/581.js
@@ -3,6 +3,7 @@
  * @return {number}
  */
 
+// 1. 先Sort 后2pointers
 var findUnsortedSubarray = function (nums) {
   // 将排序后的数组与原始数组进行比较，分别找出不一样的最左和最右的索引
   let sorted = [...nums].sort((a, b) => a - b);
@@ -20,4 +21,40 @@ var findUnsortedSubarray = function (nums) {
   }
 
   return j > i ? j - i + 1 : 0;
+};
+
+//2. 直接2pointers
+
+var findUnsortedSubarray = function (nums) {
+  let start = 0;
+  let end = nums.length - 1;
+
+  // find the first number out of sorting order from the beginning
+  while (start < nums.length - 1 && nums[start] <= nums[start + 1]) {
+    start++;
+  }
+  if (start === nums.length - 1) return 0; // if the array is sorted
+  // find the first number out of sorting order from the end
+  while (end > 0 && nums[end] >= nums[end - 1]) {
+    end--;
+  }
+
+  let subarrayMax = -Infinity,
+    subarrayMin = Infinity;
+
+  for (k = start; k < end + 1; k++) {
+    subarrayMax = Math.max(subarrayMax, nums[k]);
+    subarrayMin = Math.min(subarrayMin, nums[k]);
+  }
+
+  // extend the subarray to include any number which is bigger than the minimum of the subarray
+  while (start > 0 && nums[start - 1] > subarrayMin) {
+    start -= 1;
+  }
+  // extend the subarray to include any number which is smaller than the maximum of the subarray
+  while (end < nums.length - 1 && nums[end + 1] < subarrayMax) {
+    end += 1;
+  }
+
+  return end - start + 1;
 };
