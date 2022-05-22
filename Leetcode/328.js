9850de28e47f420d34d087b96d3764f315fbda3e
Sun flower <MLIU2@LTU.EDU>
2022-05-22 16:26:52 -0400

    add comments

--- Leetcode/328.js
+++ Leetcode/328.js
@@ -9,6 +9,7 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+//https://www.youtube.com/watch?v=PdA1uRRL40M&ab_channel=%E5%8C%97%E7%BE%8E%E7%94%9F%E6%B4%BB%E8%AF%B4
 var oddEvenList = function (head) {
   if (!head) return null;
 
