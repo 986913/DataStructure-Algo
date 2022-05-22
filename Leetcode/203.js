d8130060cdac48d895ac86b287f0f6a408fe987a
Sun flower <MLIU2@LTU.EDU>
2022-05-22 15:58:40 -0400

    203.js

--- Leetcode/203.js
+++ Leetcode/203.js
@@ -10,16 +10,33 @@
  * @param {number} val
  * @return {ListNode}
  */
+//解法1：
+var removeElements = function (head, val) {
+  let curr = head;
 
-var removeElements = function(head, val) {
+  while (curr) {
+    if (curr.val === val) {
+      curr = curr.next;
+      head = curr;
+    } else if (curr.next && curr.next.val === val) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+  return head;
+};
+
+//解法2:
+var removeElements = function (head, val) {
   let dummyHead = new ListNode(-1);
   dummyHead.next = head;
 
-  let current  = dummyHead;
+  let current = dummyHead;
 
-  while(current.next!=null){
-    if(current.next.val === val) current.next = current.next.next;
-    else current = current.next; 
+  while (current.next != null) {
+    if (current.next.val === val) current.next = current.next.next;
+    else current = current.next;
   }
   return dummyHead.next;
 };
