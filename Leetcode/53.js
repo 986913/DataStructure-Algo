1b68fe90c53632591b4cf8e7cb0d6a10433e9bb5
Sun flower <MLIU2@LTU.EDU>
2022-06-07 13:37:00 -0400

    53.js

--- Leetcode/53.js
+++ Leetcode/53.js
@@ -2,6 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
+//1. brute force: O(n^2)
 var maxSubArray = function (nums) {
   let maxSum = -Infinity;
 
@@ -11,6 +12,25 @@ var maxSubArray = function (nums) {
       sum += nums[j];
       maxSum = Math.max(maxSum, sum);
     }
+  }
+
+  return maxSum;
+};
+
+//2. 在线处理  O(N)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  let currSum = 0;
+  let maxSum = -Infinity;
+
+  for (let i = 0; i < nums.length; i++) {
+    currSum += nums[i]; /*向右累加*/
+    maxSum = Math.max(maxSum, currSum);
+    if (currSum < 0)
+      currSum = 0; /*你这当前和都成负数了，那就直接清零当前和， 抛弃之*/
   }
 
   return maxSum;
