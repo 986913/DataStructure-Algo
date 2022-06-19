865f06ff01730d0c18663d45e07e1d5435307090
Sun flower <MLIU2@LTU.EDU>
2022-06-19 13:07:16 -0400

    Update 344.js

--- Leetcode/344.js
+++ Leetcode/344.js
@@ -3,24 +3,39 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 
-
-
- const swap = (str, a, b) => {
+const swap = (str, a, b) => {
   let tmp = str[a];
   str[a] = str[b];
   str[b] = tmp;
-}
-
-/*2 pointers */
-var reverseString = function(s) {
-  let startIdx = 0; let endIdx = s.length-1;
-  
-  while(startIdx<endIdx){
-      swap(s,startIdx,endIdx);
-      startIdx++;
-      endIdx--;
-  }
-  
-  return s
 };
 
+/*2 pointers */
+var reverseString = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    swap(s, left, right);
+    left++;
+    right--;
+  }
+
+  return s;
+};
+
+/* Recurrsion + 2 pointers*/
+var reverseString = function (s) {
+  helper(0, s.length - 1, s);
+};
+
+const helper = (start, end, str) => {
+  if (start > end) return;
+  swap(str, start, end);
+  helper(start + 1, end - 1, str);
+};
+
+const swap = (str, a, b) => {
+  let tmp = str[a];
+  str[a] = str[b];
+  str[b] = tmp;
+};
