226d8d23124ee365de200e688221d40db386c0cf
Sun flower <MLIU2@LTU.EDU>
2022-07-25 21:52:33 -0400

    Update 26.js

--- Leetcode/26.js
+++ Leetcode/26.js
@@ -3,28 +3,28 @@
  * @return {number}
  */
 //注意：牵扯到数组里面 “循环”和“删除元素”时候， 最好倒着循环！！！
- var removeDuplicates = function(nums) {
-  for(let i=nums.length; i>=0; i--){
-      if(nums[i]===nums[i-1]){
-          nums.splice(i, 1);
-          
-      }
+var removeDuplicates = function (nums) {
+  for (let i = nums.length; i >= 0; i--) {
+    if (nums[i] === nums[i - 1]) {
+      nums.splice(i, 1);
+    }
   }
 };
 
 /**
- * 2 pointer 
-*/
- var removeDuplicates = function(nums) {
-    if (nums == null)  return 0;
+ * 2 pointer
+ */
+var removeDuplicates = function (nums) {
+  if (nums == null) return 0;
 
-    let writePointer = 1;
-    for(let readPointer =1; readPointer < nums.length; readPointer++){
-      if(nums[readPointer]!==nums[readPointer-1]){
-        nums[writePointer] = nums[readPointer];
-        writePointer++
-      }
+  let slow = 1;
+
+  for (let fast = 1; fast < nums.length; fast++) {
+    if (nums[fast] !== nums[slow - 1]) {
+      nums[slow] = nums[fast];
+      slow++;
     }
-    
-    return writePointer
+  }
+
+  return slow;
 };
