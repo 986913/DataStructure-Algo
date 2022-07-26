59c91c88050ffb40da8ac2e0969e25d7f1e7c01a
Sun flower <MLIU2@LTU.EDU>
2022-07-25 22:26:12 -0400

    Update 977.js

--- Leetcode/977.js
+++ Leetcode/977.js
@@ -2,30 +2,30 @@
  * @param {number[]} nums
  * @return {number[]}
  */
- var sortedSquares = function(nums) {
-  return nums.map(n=>Math.pow(n,2)).sort((a,b)=>a-b)
+var sortedSquares = function (nums) {
+  return nums.map((n) => Math.pow(n, 2)).sort((a, b) => a - b);
 };
 
-
 /* 2 pointers */
-var sortedSquares = function(arr) {
+var sortedSquares = function (arr) {
   let len = arr.length;
- let squares = Array(len).fill(0);
- let highestSquareIdx = len-1;
- let left =0; let right = len-1;
+  let squares = Array(len).fill(0);
+  let highestSquareIdx = len - 1;
+  let left = 0;
+  let right = len - 1;
 
- while(left<=right){
-   let leftSquare = Math.pow(arr[left],2);
-   let rightSquare = Math.pow(arr[right],2);
+  while (left <= right) {
+    let leftSquare = Math.pow(arr[left], 2);
+    let rightSquare = Math.pow(arr[right], 2);
 
-   if(leftSquare>rightSquare){
-     squares[highestSquareIdx] = leftSquare;
-     left++;
-   }else{
-     squares[highestSquareIdx] = rightSquare;
-     right--;
-   }
+    if (leftSquare > rightSquare) {
+      squares[highestSquareIdx] = leftSquare;
+      left++;
+    } else {
+      squares[highestSquareIdx] = rightSquare;
+      right--;
+    }
 
-   highestSquareIdx--;
- }
-}
+    highestSquareIdx--;
+  }
+};
