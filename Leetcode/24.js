56363a2dd4782464d7e88567cfbc938bda9d472a
Sun flower <MLIU2@LTU.EDU>
2022-09-15 14:57:36 -0400

    Update 24.js

--- Leetcode/24.js
+++ Leetcode/24.js
@@ -1,4 +1,4 @@
-// https://leetcode.cn/problems/swap-nodes-in-pairs/solution/shou-hua-tu-jie-24-liang-liang-jiao-huan-lian-biao/
+//https://www.bilibili.com/video/BV1YT411g7br?vd_source=2efba544aa6c1bd084ec6ddd7a98c6b2
 
 /**
  * Definition for singly-linked list.
@@ -11,22 +11,25 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
 var swapPairs = function (head) {
-  let dummy = new ListNode(-1);
-  dummy.next = head;
+  let dummyHead = new ListNode(-1, head);
+  let curr = dummyHead;
 
-  let prev = dummy;
+  /* 当链表有奇数节点时, while终止于 curr.next, 当链表有偶数节点时, while终止于 curr.next.next */
+  while (curr.next && curr.next.next) {
+    /*交换之前先存下两个tmp值*/
+    let tmp1 = curr.next;
+    let tmp2 = curr.next.next.next;
 
-  while (head && head.next) {
-    const next = head.next;
+    /*开始交换*/
+    curr.next = curr.next.next;
+    curr.next.next = tmp1;
+    tmp1.next = tmp2;
 
-    head.next = head.next.next;
-    next.next = head;
-    prev.next = next;
-
-    prev = head;
-    head = head.next;
+    //交换完后，向后移两次curr
+    curr = curr.next.next;
   }
 
-  return dummy.next;
+  return dummyHead.next;
 };
