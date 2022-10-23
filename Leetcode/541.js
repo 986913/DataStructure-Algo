ac9bcf81248870684bba9853bb8b7e24b4b67edd
Sun flower <MLIU2@LTU.EDU>
2022-10-23 01:23:37 -0400

    Update 541.js

--- Leetcode/541.js
+++ Leetcode/541.js
@@ -20,3 +20,26 @@ var reverseStr = function (s, k) {
 
   return resArr.join('');
 };
+
+/*
+function reverseStr(s: string, k: number): string {
+  const arr = s.split('');
+  
+  for (let i = 0; i < arr.length; i += 2 * k) {
+      let left = i;
+      let right = i + k - 1;
+      
+      while (left < right) {
+          if (arr[right] === undefined) {
+              right--;
+              continue;
+          }
+          
+          [arr[left], arr[right]] = [arr[right], arr[left]];
+          left++;
+          right--;
+      }
+  }
+  
+  return arr.join('');
+};*/
