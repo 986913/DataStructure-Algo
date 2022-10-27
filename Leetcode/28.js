a72ee0cc795a4a18f25e30e9381a84982a5242a9
Sun flower <MLIU2@LTU.EDU>
2022-10-27 18:05:33 -0400

    Update 28.js

--- Leetcode/28.js
+++ Leetcode/28.js
@@ -1,20 +1,20 @@
-var strStr = function(haystack, needle) {
-  if(needle==='') return 0;
-  if(haystack.length<needle.length) return -1;
-  
-  let windowStartIdx=0;
+var strStr = function (haystack, needle) {
+  if (needle === '') return 0;
+  if (haystack.length < needle.length) return -1;
+
+  let windowStartIdx = 0;
   let result = '';
   const windowSize = needle.length;
-  
-  for(let windowEndIdx = 0; windowEndIdx<haystack.length; windowEndIdx++){
-     result+=haystack[windowEndIdx];
-  
-     if(result.length>=windowSize){
-         if(result===needle) return  windowStartIdx;
-         result = result.substring(1);
-         windowStartIdx += 1;
-     }
+
+  for (let windowEndIdx = 0; windowEndIdx < haystack.length; windowEndIdx++) {
+    result += haystack[windowEndIdx];
+
+    if (result.length >= windowSize) {
+      if (result === needle) return windowStartIdx;
+      result = result.substring(1);
+      windowStartIdx += 1;
+    }
   }
-  
+
   return -1;
 };
