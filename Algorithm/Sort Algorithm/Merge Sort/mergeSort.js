1f38655f7f0fde3bacebc621781da7ae5eeccd5d
Sun flower <MLIU2@LTU.EDU>
2022-11-08 17:04:11 -0500

    Update mergeSort.js

--- Algorithm/Sort Algorithm/Merge Sort/mergeSort.js
+++ Algorithm/Sort Algorithm/Merge Sort/mergeSort.js
@@ -40,15 +40,54 @@ function merge(arr1, arr2) {
   return results;
 }
 
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
 
-function mergeSort(arr){
-  if(arr.length <= 1) return arr;
-  
-  let mid = Math.floor(arr.length/2);
+  let mid = Math.floor(arr.length / 2);
   let left = mergeSort(arr.slice(0, mid));
   let right = mergeSort(arr.slice(mid));
 
-  return merge(left, right)  // helper function
+  return merge(left, right); // helper function
 }
 
-console.log(mergeSort([8,3,99,12,1,100,74]))
+console.log(mergeSort([8, 3, 99, 12, 1, 100, 74]));
+
+/**************************************  变形题 -> https://bigfrontend.dev/problem/merge-sorted-arrays *********************************/
+function merge2Lists(arr1, arr2) {
+  let p1 = 0;
+  let p2 = 0;
+
+  let res = [];
+
+  while (p1 < arr1.length && p2 < arr2.length) {
+    if (arr1[p1] < arr2[p2]) {
+      res.push(arr1[p1]);
+      p1++;
+    } else {
+      res.push(arr2[p2]);
+      p2++;
+    }
+  }
+
+  while (p1 < arr1.length) {
+    res.push(arr1[p1]);
+    p1++;
+  }
+  while (p2 < arr2.length) {
+    res.push(arr2[p2]);
+    p2++;
+  }
+
+  return res;
+}
+function merge(arrList) {
+  if (arrList.length === 0) return [];
+  if (arrList.length === 1) return arrList[0];
+  if (arrList.length === 2) return merge2Lists(arrList[0], arrList[1]);
+
+  let mid = Math.floor(arrList.length / 2);
+  let left = merge(arrList.slice(0, mid));
+  let right = merge(arrList.slice(mid));
+
+  return merge2Lists(left, right);
+}
