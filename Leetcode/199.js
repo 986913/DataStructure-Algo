87028e143926e14e9296e6c4199143a117680c6b
Sun flower <MLIU2@LTU.EDU>
2022-11-10 17:42:55 -0500

    Update 199.js

---
+++ Leetcode/199.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+// 二叉树右视图 只需要把每一层最后一个节点存储到res数组 (是102的变形题)
+var rightSideView = function (root) {
+  if (!root) return [];
+
+  let visited = [];
+  let queue = [root];
+
+  while (queue.length) {
+    let len = queue.length;
+
+    for (let i = 0; i < len; i++) {
+      let node = queue.shift();
+
+      if (i === len - 1) {
+        //当i===len-1的时候表明到了层级最后一个节点!!
+        visited.push(node.val);
+      }
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+  }
+
+  return visited;
+};
