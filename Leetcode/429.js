547d691bab8d16b0f36658e7f21bf7240a8b1708
Sun flower <MLIU2@LTU.EDU>
2022-11-10 20:00:27 -0500

    Update 429.js

---
+++ Leetcode/429.js
@@ -0,0 +1,38 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[][]}
+ */
+
+//每一层可能有2个以上,所以不再使用node.left node.right (是102的变形题)
+var levelOrder = function (root) {
+  if (!root) return [];
+
+  let visited = [];
+  let queue = [root];
+
+  while (queue.length) {
+    let len = queue.length;
+    let currLevel = [];
+
+    for (let i = 0; i < len; i++) {
+      let node = queue.shift();
+      currLevel.push(node.val);
+      //这里不再是 ndoe.left node.right 而是循坏node.children:
+      for (let child of node.children) {
+        if (child) queue.push(child);
+      }
+    }
+
+    visited.push(currLevel);
+  }
+
+  return visited;
+};
