30286262d76b57790de991caad9db2a239dea767
Sun flower <MLIU2@LTU.EDU>
2022-11-10 19:32:00 -0500

    Update 637.js

---
+++ Leetcode/637.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+//本题就是层序遍历的时候把一层求个总和在取一个均值 (是102的变形题)
+var averageOfLevels = function (root) {
+  if (!root) return [];
+
+  let visited = [];
+  let queue = [root];
+
+  while (queue.length) {
+    let len = queue.length;
+    let currLevel = [];
+
+    for (let i = 0; i < len; i++) {
+      let node = queue.shift();
+      currLevel.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    // calculate average:
+    const currLevelAverage =
+      currLevel.reduce((acc, cur) => acc + cur) / currLevel.length;
+    visited.push(currLevelAverage);
+  }
+
+  return visited;
+};
