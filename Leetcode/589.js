f0731843104ddf0ceb5a09562b1ca15c5c0587b3
Sun flower <MLIU2@LTU.EDU>
2022-11-11 20:41:00 -0500

    n叉树 dfs

---
+++ Leetcode/589.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a Node:
+ *
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+
+//solution1:     BFS - recursion     preorder: 中左右
+var preorder = function (root) {
+  let visited = [];
+
+  const helper = (node) => {
+    if (!node) return;
+
+    visited.push(node.val);
+    //这里不再是nide.left, node.right了， 而是循环node.children
+    for (let child of node.children) {
+      helper(child);
+    }
+  };
+
+  helper(root);
+  return visited;
+};
+
+//solution1:     BFS - itelation
---
+++ Leetcode/590.js
@@ -0,0 +1,31 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+
+//solution1:     BFS - recursion     postorder: 左右中
+var postorder = function (root) {
+  let visited = [];
+  const helper = (node) => {
+    if (!node) return;
+
+    //这里不再是nide.left, node.right了， 而是循环node.children
+    for (let child of node.children) {
+      helper(child);
+    }
+    visited.push(node.val);
+  };
+
+  helper(root);
+  return visited;
+};
+
+//solution1:     BFS - itelation
