25c5b577e7cf776c123a3ddec652f5d1c1f08835
Sun flower <MLIU2@LTU.EDU>
2022-11-12 15:07:01 -0500

    Update 101.js

--- Leetcode/101.js
+++ Leetcode/101.js
@@ -11,22 +11,29 @@
  * @return {boolean}
  */
 
+/**
+ *
+ * 什么样的题目一定要使用post order遍历？
+ *  - 需要收集孩子的信息，向上一层返回的题目 比如下面这个题
+ */
+
+//https://www.bilibili.com/video/BV1ue4y1Y7Mf/?vd_source=2efba544aa6c1bd084ec6ddd7a98c6b2
+
+//solution1:  使用递归遍历左右子树 递归三部曲 （这道题只能 post_order:左右中）
 var isSymmetric = function (root) {
-  if (root == null) return true;
-  return isSymmetricHelper(root.left, root.right);
-};
+  // 1. 确定递归的参数 root.left root.right和返回值true false：  就是判断两个二叉树镜面反转后是否一样 （分别以left和right为root的两个二叉树）
+  const compareNode = (left, right) => {
+    //2. 确定终止条件 空的情况
+    if (left === null && right !== null) return false;
+    else if (left !== null && right === null) return false;
+    else if (left === null && right === null) return true;
+    else if (left.val !== right.val) return false;
+    //3. 当left.val==-right.val, 确定单层递归逻辑:
+    let outside = compareNode(left.left, right.right); // 左/右
+    let inside = compareNode(left.right, right.left); // 右/左
+    let result = outside && inside; //中
+    return result;
+  };
 
-const isSymmetricHelper = (left, right) => {
-  //有且仅有一个为 null ，直接返回 false
-  if ((left == null && right != null) || (left != null && right == null)) return false;
-
-  if (left != null && right != null) {
-    //A 的根节点和 B 的根节点是否相等
-    if (left.val != right.val) return false;
-    //A 的左子树和 B 的右子树是否相等，同时 A 的右子树和左子树是否相等。
-    return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left)
-  }
-
-  //都为 null，返回 true
-  return true;
+  return compareNode(root.left, root.right);
 };
