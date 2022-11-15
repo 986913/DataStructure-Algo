0adcb08809a17cadbc1d68769b34d5b8bb6b9b7d
Sun flower <MLIU2@LTU.EDU>
2022-11-14 20:40:57 -0500

    Update 34.js

--- Leetcode/34.js
+++ Leetcode/34.js
@@ -3,55 +3,53 @@
  * @param {number} target
  * @return {number[]}
  */
-const findFirstPosition = (nums, target) => {
+
+const left_bound = (nums, target) => {
   let left = 0;
   let right = nums.length - 1;
-
-  let index = -1;
 
   while (left <= right) {
     let mid = left + Math.floor((right - left) / 2);
 
-    if (target <= nums[mid]) {
-      //从模板的<变成了<=
+    if (target > nums[mid]) {
+      left = mid + 1;
+    } else if (target < nums[mid]) {
       right = mid - 1;
     } else {
-      left = mid + 1;
-    }
-    if (target === nums[mid]) {
-      index = mid;
+      right = mid - 1; //别返回，要收缩右边界
     }
   }
 
-  return index;
+  //最后要检查left越界情况
+  if (left >= nums.length || nums[left] !== target) return -1;
+
+  return left;
 };
-
-const findLastPosition = (nums, target) => {
+const right_bound = (nums, target) => {
   let left = 0;
   let right = nums.length - 1;
-
-  let index = -1;
 
   while (left <= right) {
     let mid = left + Math.floor((right - left) / 2);
 
-    if (target >= nums[mid]) {
+    if (target > nums[mid]) {
       left = mid + 1;
-    } else {
+    } else if (target < nums[mid]) {
       right = mid - 1;
-    }
-
-    if (target === nums[mid]) {
-      index = mid;
+    } else {
+      left = mid + 1; //别返回，要收缩左边界
     }
   }
 
-  return index;
+  //最后要检查left越界情况
+  if (right < 0 || nums[right] !== target) return -1;
+
+  return right;
 };
 
 var searchRange = function (nums, target) {
   let result = [];
-  result[0] = findFirstPosition(nums, target);
-  result[1] = findLastPosition(nums, target);
+  result[0] = left_bound(nums, target);
+  result[1] = right_bound(nums, target);
   return result;
 };
