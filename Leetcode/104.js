cd629c85a4e97cef47517d116522ff3ec503d202
Sun flower <MLIU2@LTU.EDU>
2022-11-14 20:20:23 -0500

    Update 104.js

--- Leetcode/104.js
+++ Leetcode/104.js
@@ -56,15 +56,13 @@ var maxDepth = function (root) {
   return height;
 };
 
-/**
+/**--- Solution2 👍 --- post_order DFS  - 使用递归三部曲 ------------------------------------------------
  * https://www.bilibili.com/video/BV1Gd4y1V75u/?vd_source=2efba544aa6c1bd084ec6ddd7a98c6b2
- *
  *
  * 二叉树节点的深度(depth)：指从根节点到该节点的最长简单路径边的条数。 求深度(depth)可以从上到下去查 所以需要pre_order前序遍历（中左右）
  * 二叉树节点的高度(height)：指从该节点到叶子节点的最长简单路径边的条数。 求高度(height)只能从下到上去查，所以只能post_order后序遍历（左右中）
  *
  * 那么,为什么这道题求的是二叉树的最大深度，也用的是后序遍历， 那是因为代码的逻辑其实是求的根节点的高度，而根节点的高度就是这棵树的最大深度，所以才可以使用后序遍历
- * Solution2 👍 --- post_order DFS  - 使用递归三部曲:
  */
 var maxDepth = function (root) {
   //1.确定递归函数的参数和返回值: 参数就是传入树的根节点，返回就返回这棵树的深度
