cf39c1a0cb6142ea63f931c2f3af84df6bd60420
Sun flower <MLIU2@LTU.EDU>
2022-11-14 19:20:15 -0500

    Update 104.js

--- Leetcode/104.js
+++ Leetcode/104.js
@@ -13,7 +13,7 @@
 
 //https://www.bilibili.com/video/BV1Gd4y1V75u/?vd_source=2efba544aa6c1bd084ec6ddd7a98c6b2
 
-//BFS: 是102的变形题,只是输出visited.length就行
+// --- Solution1.1 --- BFS: 是102的变形题,只是输出visited.length就行 ------------------------------------
 /*
 var maxDepth = function (root) {
   if (!root) return [];
@@ -39,7 +39,7 @@ var maxDepth = function (root) {
 }; 
 */
 
-//BFS: 是102的变形题 --- Solution1
+// --- Solution1.2 --- BFS: 是102的变形题 ------------------------------------------------
 var maxDepth = function (root) {
   if (!root) return [];
 
@@ -59,8 +59,7 @@ var maxDepth = function (root) {
   return height;
 };
 
-//post_order DFS  - 使用递归的方法 递归三部曲 - solution2
-
+// --- Solution2 --- post_order DFS  - 使用递归的方法 递归三部曲------------------------------------------
 var maxDepth = function (root) {
   //1.确定递归函数的参数和返回值: 参数就是传入树的根节点，返回就返回这棵树的深度
   const getDepth = (node) => {
