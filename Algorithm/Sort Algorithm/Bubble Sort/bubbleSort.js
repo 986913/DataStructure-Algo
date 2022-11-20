23b899515688b3a36fa4efc2fa5518fe149a617c
Sun flower <MLIU2@LTU.EDU>
2022-11-20 16:55:45 -0500

    Update bubbleSort.js

--- Algorithm/Sort Algorithm/Bubble Sort/bubbleSort.js
+++ Algorithm/Sort Algorithm/Bubble Sort/bubbleSort.js
@@ -5,63 +5,41 @@
   博客解说： https://www.cnblogs.com/echolun/p/12638903.html
 */
 
-
-
 /* bubble sort's dummy version: */
-function sort(arr){
-  for(let i=0; i<arr.length-1; i++){ 
-    for(let j=0; j<arr.length-1; j++){
-      if(arr[j] > arr[j+1]){ 
-        // swap :
-        let tmp = arr[j+1];
-        arr[j+1] = arr[j];
-        arr[j] = tmp;
+function sort(arr) {
+  //外层-->遍历次数
+  for (let i = 0; i < arr.length - 1; i++) {
+    //内层-->比较次数
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]]; // swap arr[j+1] and arr[j]
       }
     }
   }
   return arr;
 }
 
-
-/* bubble sort's effient version: 注意循环条件*/
-function sort(arr){
-  for(let i = 0; i <arr.length-1; i++){ 
-    for(let j = 0; j < arr.length-1-i; j++){
-      if(arr[j] > arr[j+1]){ 
-        // swap :
-        let tmp = arr[j+1];
-        arr[j+1] = arr[j];
-        arr[j] = tmp;
-      }
-    }
-  }
-  return arr;
-}
-
-
-/* bubble sort's most effient version: done 对输入是那种差不多已经快排好序的 有大的性能提升 
+/* bubble sort's effient version: done对输入是那种差不多已经快排好序的 有大的性能提升 
       形如[8,1,2,3,4,5,6,7]这种数组，只需比较一轮就已经排好了。
-      所以我们加一个标志位，每次外层循环开始前将标志位置为true，内层循环过程中，出现两数交换时，置为false。
+      所以我们加一个flag，每次外层循环开始前将标志位置为true，内层循环过程中，出现两数交换时，置为false。
       如果内层循环没有任何两数进行交换，标志位为true，表示排序完成。 这样我们就可以减少不必要的排序，提高性能
 */
-function sort(arr){
+function sort(arr) {
   let done;
-  for(let i = 0; i <arr.length-1; i++){ 
+
+  for (let i = 0; i < arr.length - 1; i++) {
     done = true;
-    for(let j = 0; j < arr.length-1-i; j++){
-      if(arr[j] > arr[j+1]){ 
-        // swap :
-        let tmp = arr[j+1];
-        arr[j+1] = arr[j];
-        arr[j] = tmp;
+
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]]; // swap arr[j+1] and arr[j]
         done = false;
       }
     }
-    if(done) break;
+    if (done) break;
   }
   return arr;
 }
 
-
-sort([100,23,6,0,99,45])  // [0,6,23,45,99, 100]
-sort([8,1,2,3,4,5,6,7])   // [1,2,3,4,5,6,7,8]
+sort([100, 23, 6, 0, 99, 45]); // [0,6,23,45,99, 100]
+sort([8, 1, 2, 3, 4, 5, 6, 7]); // [1,2,3,4,5,6,7,8]
