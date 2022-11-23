de387d48a7827ddc0d3453652b31470ebbf37137
Sun flower <MLIU2@LTU.EDU>
2022-11-22 21:07:23 -0500

    Update 541.js

--- Leetcode/541.js
+++ Leetcode/541.js
@@ -1,5 +1,3 @@
-//这道题没有算法
-
 /**
  * @param {string} s
  * @param {number} k
@@ -9,9 +7,10 @@
 function reverseStr(s, k) {
   const arr = s.split('');
 
+  // 每计数至 2k 个字符 -> foreach use 2K as step
   for (let i = 0; i < arr.length; i += 2 * k) {
     let left = i;
-    let right = i + k - 1;
+    let right = i + k - 1; //就反转这 2k 个字符中的前 k 个字符 --> 就确定了right的开始位置(相对于left+k的位置)
 
     while (left < right) {
       [arr[left], arr[right]] = [arr[right], arr[left]];
