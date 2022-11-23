0dcf478c761bd920c2438e027d5b23246e8bc2ed
Sun flower <MLIU2@LTU.EDU>
2022-11-23 15:00:15 -0500

    commnets

--- Algorithm/Sort/Selection Sort/selectionSort.js
+++ Algorithm/Sort/Selection Sort/selectionSort.js
@@ -1,34 +1,31 @@
 /* 
   Selection Sort: 没有bubble sort和insertion sort好使
 
-  sort([8, 1, 2, 3, 4, 5, 6, 7]); // [1,2,3,4,5,6,7,8]
-
   Big O:
-    best time complexity     O(n)
+    best time complexity     O(n2)
     average time complexity  O(n2)
     worst time complexity    O(n2)
 
     space complexity         O(1)
 */
 
-// https://www.youtube.com/watch?v=xWBP4lzkoyM&ab_channel=GeeksforGeeks
-
 function selectionSort(arr) {
+  //外部循环控制走的轮数(arr.length)
   for (let i = 0; i < arr.length; i++) {
     let minIndex = i;
 
+    // 内循环负责找出最小值的index.
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[j] < arr[minIndex]) minIndex = j;
     }
 
-    if (i !== minIndex) {
-      let tmp = arr[minIndex];
-      arr[minIndex] = arr[i];
-      arr[i] = tmp;
+    //每走完一轮,如果最小值的index和sorting index不一样 则进行swap.
+    if (minIndex !== i) {
+      [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]];
     }
   }
 
   return arr;
 }
 
-selectionSort([19, 5, 25, 3, 10]);
+//selectionSort([19, 5, 25, 3, 10]); //[3,5,10,19,25]
