19e4b1cd4edb9c89a55770f04b851b640891cba4
Sun flower <MLIU2@LTU.EDU>
2022-11-23 13:36:31 -0500

    Update 459.js

--- Leetcode/459.js
+++ Leetcode/459.js
@@ -1,5 +1,6 @@
+// solution 1: 移动匹配: 判断字符串s是否有重复子串组成，只要两个s拼接在一起，里面还出现一个s的话，就说明是有重复子串组成。
 var repeatedSubstringPattern = function (s) {
   const repeated = s.repeat(2); // repeat s
-  const newstr = repeated.slice(1, -1); //  去除掉首尾char
+  const newstr = repeated.slice(1, -1); //  去除掉首尾char, 这样避免在s+s中搜索出原来的s，我们要搜索的是中间拼接出来的s。
   return newstr.includes(s);
 };
