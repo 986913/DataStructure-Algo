dc1c20d96228b4ad818feaddfc89397a26f31645
Sun flower <MLIU2@LTU.EDU>
2022-11-25 16:39:31 -0500

    Update 1047.js

--- Leetcode/1047.js
+++ Leetcode/1047.js
@@ -6,14 +6,13 @@ var removeDuplicates = function (s) {
   let stack = [];
 
   for (let char of s) {
-    if (stack.length === 0) {
+    if (char !== stack[stack.length - 1]) {
       stack.push(char);
     } else {
-      const top = stack[stack.length - 1];
-      if (top === char) stack.pop();
-      else stack.push(char);
+      stack.pop();
     }
   }
+
   return stack.join('');
 };
 console.log(removeDuplicates('abbaca')); //'ca'
