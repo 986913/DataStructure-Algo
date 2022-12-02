a28f57e91ef1178a1330e5a5ff2a0e3c06e2d987
Sun flower <MLIU2@LTU.EDU>
2022-12-02 17:22:46 -0500

    constructor

--- Leetcode/105.js
+++ Leetcode/105.js
@@ -12,16 +12,21 @@
  * @return {TreeNode}
  */
 
-// 👍
+// 👍 https://www.bilibili.com/video/BV1vW4y1i7dn/?vd_source=2efba544aa6c1bd084ec6ddd7a98c6b2
+
 var buildTree = function (preorder, inorder) {
   if (!preorder.length) return null;
 
   const rootVal = preorder.shift(); // 从前序遍历的数组中获取中间节点的值， 即数组第一个值
+
   const index = inorder.indexOf(rootVal); // 获取中间节点在中序遍历中的下标
+
   const root = new TreeNode(rootVal); // 创建中间节点
 
-  root.left = buildTree(preorder.slice(0, index), inorder.slice(0, index)); // 创建左节点
-  root.right = buildTree(preorder.slice(index), inorder.slice(index + 1)); // 创建右节点
+  //创建左节点
+  root.left = buildTree(preorder.slice(0, index), inorder.slice(0, index));
+  // 创建右节点
+  root.right = buildTree(preorder.slice(index), inorder.slice(index + 1)); // 注意inorder: rootIndex + 1 here
 
   return root;
 };
--- Leetcode/106.js
+++ Leetcode/106.js
@@ -12,24 +12,28 @@
  * @return {TreeNode}
  */
 
-// 👍
+// 👍 https://www.bilibili.com/video/BV1vW4y1i7dn/?vd_source=2efba544aa6c1bd084ec6ddd7a98c6b2
 
 var buildTree = function (inorder, postorder) {
   if (!inorder.length) return null;
 
   const rootVal = postorder.pop(); // 从后序遍历的数组中获取中间节点的值， 即数组最后一个值
+
   let rootIndex = inorder.indexOf(rootVal); // 获取中间节点在中序遍历中的下标
+
   const root = new TreeNode(rootVal); // 创建中间节点
 
+  // 创建左节点
   root.left = buildTree(
     inorder.slice(0, rootIndex),
     postorder.slice(0, rootIndex)
-  ); // 创建左节点
+  );
 
+  // 创建右节点
   root.right = buildTree(
-    inorder.slice(rootIndex + 1),
+    inorder.slice(rootIndex + 1), // 注意inorder: rootIndex + 1 here
     postorder.slice(rootIndex)
-  ); // 创建右节点
+  );
 
   return root;
 };
