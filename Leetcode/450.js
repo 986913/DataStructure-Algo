3e505cecf86529caccc7813a01a61705249cf791
Sun flower <MLIU2@LTU.EDU>
2022-12-06 19:13:16 -0500

    Update 450.js

--- Leetcode/450.js
+++ Leetcode/450.js
@@ -13,7 +13,8 @@
  */
 
 /**
-递归终止条件：
+  二叉搜索树中删除节点遇到的情况：
+
   case 1. 没找到要删除的node
   case 2. 要删除的node是叶子节点
   case 3. 要删除的node，左子树不为空，右为空
@@ -29,6 +30,7 @@ const getMinNode = (root) => {
 };
 
 var deleteNode = function (root, key) {
+  /* 确定递归终止条件: 遇到空返回 其实这也说明没找到删除的节点，遍历到空节点直接返回了 */
   if (!root) return null; //case 1. 没找到要删除的node
 
   if (key > root.val) {
@@ -38,16 +40,16 @@ var deleteNode = function (root, key) {
     root.left = deleteNode(root.left, key);
     return root;
   } else {
-    if (!root.left && !root.right) return null; //case 2. 要删除的node是叶子节点
+    if (!root.left && !root.right) return null; //case 2. 要删除的node是叶子节点,返回NULL为根节点
 
     if (root.left && !root.right)
-      return root.left; //case 3. 要删除的node，左子树不为空，右为空
-    else if (!root.left && root.right) return root.right; //case 4. 要删除的node，右子树不为空，左为空
+      return root.left; //case 3. 要删除的node左子树不为空，右为空 则 移除节点，左孩子补位，返回左孩子为根节点
+    else if (!root.left && root.right) return root.right; //case 4. 要删除的node右子树不为空，左为空 则 移除节点,右孩子补位,返回右孩子为根节点
 
     //case 5. 要删除的node左右子树都不为空
     let min = getMinNode(root.right); // 找出要删除的node右子树中的最小node (也就是min变量)
     min.left = root.left; // 再安排要删除的node左子树到min的左子树位置上
-    //再移除root
+    //再移除root， 返回删除节点右孩子为新的根节点。
     return root.right;
   }
 };
