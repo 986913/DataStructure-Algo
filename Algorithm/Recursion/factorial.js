d7535e28b921054bcac9ffd829864e342fbbae12
Sun flower <MLIU2@LTU.EDU>
2022-12-25 20:44:44 -0500

    recursion

--- Algorithm/Recursion/factorial.js
+++ Algorithm/Recursion/factorial.js
@@ -7,7 +7,6 @@ eg: factorial(4) // 24   解释：4x3x2x1=24
     factorial(5) // 120  解释：5x4x3x2x1=120
 */
 
-
 // 迭代法：
 // const factorial = n => {
 //   let result = 1;
@@ -18,9 +17,11 @@ eg: factorial(4) // 24   解释：4x3x2x1=24
 // }
 
 // recurrsion:
-const factorial = n => {
-  if(n===1) return 1
-  return n*factorial(n-1);
-}
+const factorial = (n) => {
+  if (n === 1) return 1;
+  return n * factorial(n - 1);
+};
 
-factorial(5) // 120
+factorial(5); // 120
+
+//求时间复杂度: 由于 f(n) = n * f(n-1) = n * (n-1) * .... * f(1),总共作了 n 次乘法，所以时间复杂度为 n。
