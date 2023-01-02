e56644340e5ab56c8805016f037cadc3987eda03
Sun flower <MLIU2@LTU.EDU>
2023-01-02 16:27:29 -0500

    Update findGcd.js

--- Algorithm/Recursion/findGcd.js
+++ Algorithm/Recursion/findGcd.js
@@ -1,3 +1,19 @@
 /**
  * Implement a function that takes two numbers, testVariable1 and testVariable2 and returns their greatest common divisor.
  */
+
+function gcd(testVariable1, testVariable2) {
+  if (testVariable1 === testVariable2) return testVariable1;
+
+  if (testVariable1 > testVariable2) {
+    return gcd(testVariable1 - testVariable2, testVariable2);
+  } else {
+    return gcd(testVariable2 - testVariable1, testVariable1);
+  }
+}
+
+/**
+ * gcd(56, 42)	-->  14
+ * gcd(6, 9)	  -->  3
+ * gcd(14, 30)	-->  2
+ **/
