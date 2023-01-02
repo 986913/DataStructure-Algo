c937c9e41c5c537ed3eb9eb0c1e9f50e90f481c1
Sun flower <MLIU2@LTU.EDU>
2023-01-02 16:29:46 -0500

    Update findGcd.js

--- Algorithm/Recursion/findGcd.js
+++ Algorithm/Recursion/findGcd.js
@@ -3,8 +3,9 @@
  */
 
 function gcd(testVariable1, testVariable2) {
-  if (testVariable1 === testVariable2) return testVariable1;
+  if (testVariable1 === testVariable2) return testVariable1; // 终止条件
 
+  // 单层递归逻辑
   if (testVariable1 > testVariable2) {
     return gcd(testVariable1 - testVariable2, testVariable2);
   } else {
