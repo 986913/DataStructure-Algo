c9790fdb1b4231fea90b78f5e9c858efc328b3de
Sun flower <MLIU2@LTU.EDU>
2023-01-13 18:53:27 -0500

    Update 5.js

--- Leetcode/5.js
+++ Leetcode/5.js
@@ -3,16 +3,16 @@
  * @return {string}
  */
 
-/* Soultion1:  自己写的暴力法。 时间耗时太长 不推荐用 */
+/* ------------------------- Soultion1:  自己写的暴力法。 时间耗时太长 不推荐用 ------------------------------------------ */
 var longestPalindrome = function (s) {
-  const isPalindromic = (str) => str === str.split("").reverse().join("");
+  const isPalindromic = (str) => str === str.split('').reverse().join('');
 
   let palindromicArr = []; //holds all palindromic combo
 
   for (let i = 0; i < s.length; i++) {
     for (let j = 0; j <= s.length; j++) {
       let subStr = s.slice(i, j);
-      if (isPalindromic(subStr) && subStr !== "") {
+      if (isPalindromic(subStr) && subStr !== '') {
         palindromicArr.push(subStr);
       }
     }
@@ -20,7 +20,7 @@ var longestPalindrome = function (s) {
 
   // return most longest one of palindromicArr
   let maxLen = 0;
-  let result = "";
+  let result = '';
   palindromicArr.forEach((item) => {
     if (item.length > maxLen) {
       maxLen = item.length;
@@ -30,7 +30,34 @@ var longestPalindrome = function (s) {
   return result;
 };
 
-/* Soulution2: DP */
+// ------------------------- 2 pointers: -----------------------------------------------------------------------*/
+
+//寻找最长回文串: 从str[left]和str[right]开始向两端扩散，返回以str[left]和str[right]为中心的最长回文串
+const palindrome = (str, left, right) => {
+  //防止索引越界
+  while (left >= 0 && right < str.length && str[left] === str[right]) {
+    left--;
+    right++;
+  }
+  return str.substr(left + 1, right - left - 1);
+};
+
+const longestPalindrome = (s) => {
+  let result = '';
+
+  for (let i = 0; i < s.length; i++) {
+    let s1 = palindrome(s, i, i); //寻找长度为奇数的回文字串
+    let s2 = palindrome(s, i, i + 1); //寻找长度为偶数的回文字串
+
+    //result = longest(res, s1, s2)
+    result = result.length > s1.length ? result : s1;
+    result = result.length > s2.length ? result : s2;
+  }
+
+  return result;
+};
+
+/* ------------------------- Soulution3: DP ----------------------------------------------------------------------- */
 /* 
   维护一个dp维数组,  dp[i][j]的i,j代表string的index, dp[i][j]存的是true/false,代表是不是palindrome
   https://leetcode.com/problems/longest-palindromic-substring/discuss/1167311/JavaScript-solution-Dynamic-Programming-Time-O(n2)-Space-O(n2)-with-Explanation 
@@ -82,3 +109,5 @@ var longestPalindrome = function (s) {
   }
   return palindrome;
 };
+
+/* solution 3: two pointer  --  写不下去可参考labuladong */
