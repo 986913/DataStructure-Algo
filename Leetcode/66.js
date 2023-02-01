517a6d68a5e98377912fed3c7a7ed57411df0ade
Sun flower <MLIU2@LTU.EDU>
2023-01-31 20:11:14 -0500

    5

--- Leetcode/66.js
+++ Leetcode/66.js
@@ -3,18 +3,15 @@
  * @return {number[]}
  */
 
- var plusOne = function(digits) {
- 
-  for(let i=digits.length-1; i>=0; i--){
-     if(digits[i]<9){
-         digits[i]+=1;
-         return digits
-     }else {
-       digits[i]=0
-     }
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i] += 1;
+      return digits;
+    } else {
+      digits[i] = 0;
+    }
   }
   // make [9,9,9], [1,0,0,0] not [0,0,0]
-  return [1, ...digits]
+  return [1, ...digits];
 };
-
-
