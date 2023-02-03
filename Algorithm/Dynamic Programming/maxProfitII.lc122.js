6c7df676d21be0a305edc90b11c51062902263dc
Sun flower <MLIU2@LTU.EDU>
2023-02-03 11:23:49 -0500

    trim

--- Algorithm/Dynamic Programming/maxProfitII.lc122.js
+++ Algorithm/Dynamic Programming/maxProfitII.lc122.js
@@ -44,10 +44,8 @@ var maxProfit = function (prices) {
 
   //更新dp[i]
   for (let i = 1; i < len; i++) {
-    dp[i] = [
-      Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]),
-      Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]),
-    ];
+      dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]),
+      dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]),
     /* dp[i][0] ----> 如果第i天持有股票，那由两个状态推出来:
           第i-1天就持有股票，那么就保持现状，所得现金就是昨天持有股票的所得现金 即：dp[i - 1][0]
           第i天买入股票，所得现金就是昨天不持有股票的所得现金减去 今天的股票价格 即：dp[i - 1][1] - prices[i] 
--- Leetcode/1.js
+++ Leetcode/1.js
@@ -17,26 +17,7 @@ var twoSum = function (nums, target) {
   return result;
 };
 
-//2. 👍👍👍 sort+2points: 定位两个指针根据和的大小来移动另外一个。这里设定的指针个数根据题目中K的个数来定
-var twoSum = function (nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
-
-  while (left < right) {
-    let sum = nums[left] + nums[right];
-    if (sum === target) {
-      return [left, right];
-      // left ++; right++
-    } else if (sum < target) {
-      left++;
-    } else if (sum > target) {
-      right--;
-    }
-  }
-  return [-1, -1];
-};
-
-/* 👍👍👍solution3: hash map:  
+/* 👍👍👍solution2: hash map:  
     遍历数组，每访问一个元素，先判断其配对的元素是否在Hash表中，hash table以item为Key, index为value
     如果在的话就说明我们找到了答案，将其输出即可，
     如果没有找到，就将当前的元素放入 Hash 表中，以方便后面的元素来配对。
@@ -46,8 +27,30 @@ var twoSum = function (nums, target) {
 
   for (let i = 0; i < nums.length; i++) {
     let diff = target - nums[i];
-
-    if (seen.has(diff)) return [i, seen.get(diff)]; // 先判断diff是否被遍历过， 被遍历过得话就直接返回了
+    // 先判断diff是否被遍历过， 被遍历过得话就直接返回了
+    if (seen.has(diff)) {
+      return [i, seen.get(diff)];
+    }
     seen.set(nums[i], i);
   }
+
+  return [-1, -1];
+};
+
+//3. 👍 sort+2points: 定位两个指针根据和的大小来移动另外一个。这里设定的指针个数根据题目中K的个数来定
+var twoSum = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let sum = nums[left] + nums[right];
+    if (sum === target) {
+      return [left, right]; // left ++; right++
+    } else if (sum < target) {
+      left++;
+    } else if (sum > target) {
+      right--;
+    }
+  }
+  return [-1, -1];
 };
