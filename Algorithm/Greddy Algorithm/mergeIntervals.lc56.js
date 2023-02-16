5a504cbbf12d0fbca8cbe95d2c7e554a8d2423bc
Sun flower <MLIU2@LTU.EDU>
2023-02-15 20:46:22 -0500

    day12

--- Algorithm/Greddy Algorithm/mergeIntervals.lc56.js
+++ Algorithm/Greddy Algorithm/mergeIntervals.lc56.js
@@ -4,6 +4,7 @@
  */
 
 /**
+ * --------------------------Greedy--------------------------------------
  * 我按照左边界排序，排序之后局部最优：每次合并都取最大的右边界，这样就可以合并更多的区间了，
  * 整体最优：合并所有重叠的区间。
  */
@@ -16,7 +17,8 @@ var merge = function (intervals) {
     if (result.length === 0) {
       result.push(curr);
     } else {
-      let prev = result[result.length - 1];
+      let prev = result[result.length - 1]; // --> 注意prev是从result中取的。。
+
       //curr的左边界<=prev的右边界，则说明overlap了。那就得合并取最大的右边界
       if (curr[0] <= prev[1]) {
         prev[1] = Math.max(prev[1], curr[1]);
