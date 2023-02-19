7e2dbbdb9c0418d87b2e5d246ef2666b3462ea17
Sun flower <MLIU2@LTU.EDU>
2023-02-19 15:41:37 -0500

    Update 143.js

--- Leetcode/143.js
+++ Leetcode/143.js
@@ -9,6 +9,20 @@
  * @param {ListNode} head
  * @return {void} Do not return anything, modify head in-place instead.
  */
+/**
+This problem is a combination of these three easy problems:
+  Middle of the Linked List.
+  Reverse Linked List.
+  Merge Two Sorted Lists.
+  -------------------------------------------------------------------
+  Find a middle node of the linked list. If there are two middle nodes, return the second middle node. 
+    (Example: for the list 1->2->3->4->5->6, the middle element is 4.)
+  Once a middle node has been found, reverse the second part of the list. 
+    (Example: convert 1->2->3->4->5->6 into 1->2->3->4 and 6->5->4.)
+  Now merge the two sorted lists. 
+    (Example: merge 1->2->3->4 and 6->5->4 into 1->6->2->5->3->4.)
+ */
+
 const reverse = (head) => {
   let prev = null;
   let next = head;
@@ -37,7 +51,6 @@ var reorderList = function (head) {
 
   //3. reset fast: 这时候fast就是list前半段了
   fast = head;
-  // console.log(fast, slow)
 
   //4. mergege fast and slow into a one
   while (slow.next !== null) {
