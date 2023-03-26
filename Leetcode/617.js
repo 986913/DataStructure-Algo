8bd472f98d17f783ddfc4fe042a13d53b8473013
Sun flower <MLIU2@LTU.EDU>
2023-03-26 16:23:29 -0400

    617

--- Leetcode/617.js
+++ Leetcode/617.js
@@ -13,14 +13,20 @@
  */
 
 //递归参数是要传入两个二叉树的根节点，返回值就是合并之后二叉树的根节点。
+
 var mergeTrees = function (root1, root2) {
   //确定终止条件：
-  if (root1 === null) return root2;
-  if (root2 === null) return root1;
+  if (root1 && !root2) return root1;
+  if (!root1 && root2) return root2;
+  if (!root1 && !root2) return null;
 
   // 确定单层递归的逻辑：
-  let newRoot = new TreeNode(root1.val + root2.val);
+  const sum = root1.val + root2.val;
+  const newRoot = new TreeNode(sum);
+
+  //构造左子树
   newRoot.left = mergeTrees(root1.left, root2.left);
+  //构造右子树
   newRoot.right = mergeTrees(root1.right, root2.right);
 
   return newRoot;
