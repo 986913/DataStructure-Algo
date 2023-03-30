410e5f07ed87c338d84dc01c3d70df4ee30fb2a1
Sun flower <MLIU2@LTU.EDU>
2023-03-30 14:45:45 -0400

    710

--- Leetcode/701.js
+++ Leetcode/701.js
@@ -12,39 +12,45 @@
  * @return {TreeNode}
  */
 
-//  -------------------------------------- Solution 1: DFS Recursion ------------------------------------
+/*  -------------------------------------- Solution 1: DFS Recursion ------------------------------------ */
 
 var insertIntoBST = function (root, val) {
   if (!root) return new TreeNode(val); //找到了插入位置(叶子节点)，然后创建且返回新节点
 
-  if (root.val > val) {
+  if (val < root.val) {
     root.left = insertIntoBST(root.left, val); // 在这连接上了新节点和父亲节点，新建节点作为父节点左子树 --> root.left子树
-  }
-
-  if (root.val < val) {
+  } else {
     root.right = insertIntoBST(root.right, val); //在这连接上了新节点和父亲节点，新建节点作为父节点右子树 --> root.right子树
   }
 
   return root;
 };
 
-//  -------------------------------------- Solution 2: Iteration ------------------------------------
+/*  -------------------------------------- Solution 2: Iteration ------------------------------------ */
 var insertIntoBST = function (root, val) {
   if (!root) return new TreeNode(val);
 
   let parent = null; //记录插入的节点
+  let curr = root; // copy一份root为curr, 遍历curr而不直接遍历root
+  let newNode = new TreeNode(val);
 
-  let curr = root;
   while (curr) {
-    parent = curr; //update插入的节点，直到叶子节点
-    if (curr.val > val) curr = curr.left;
-    else curr = curr.right;
+    parent = curr; // update 插入的节点
+
+    if (val > curr.val) {
+      curr = curr.right;
+    } else {
+      curr = curr.left;
+    }
   }
 
-  let newnode = new TreeNode(val);
-  // 开始插入：
-  if (parent.val > val) parent.left = newnode;
-  else parent.right = newnode;
+  //找到插入的节点后，利用BST的特性开始插入新节点
+  if (parent.val > val) {
+    parent.left = newNode;
+  } else {
+    parent.right = newNode;
+  }
 
+  //返回插好的root
   return root;
 };
