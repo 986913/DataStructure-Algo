743557375b14dfbee27488d47d044d56a5483e65
Sun flower <MLIU2@LTU.EDU>
2023-04-09 10:16:25 -0400

    push

--- Leetcode/14.js
+++ Leetcode/14.js
@@ -2,16 +2,16 @@
  * @param {string[]} strs
  * @return {string}
  */
- var longestCommonPrefix = function(strs) {
-  if(strs.length===0) return '';
+var longestCommonPrefix = function (strs) {
+  if (strs.length === 0) return '';
   let prefix = strs[0];
-  
-  for(let i=1; i<strs.length; i++){
-      while(strs[i].indexOf(prefix) !==0 ){
-          prefix=prefix.substring(0, prefix.length-1);
-          // console.log(prefix)
-      }
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+      // console.log(prefix)
+    }
   }
-  
-  return prefix
+
+  return prefix;
 };
