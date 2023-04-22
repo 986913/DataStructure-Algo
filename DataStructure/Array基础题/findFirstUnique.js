dbbb707e16f1b9404bd7ccd7f45df87a3d853699
Sun flower <MLIU2@LTU.EDU>
2023-04-22 17:09:27 -0400

    add one more

--- DataStructure/Array基础题/findFirstUnique.js
+++ DataStructure/Array基础题/findFirstUnique.js
@@ -11,3 +11,25 @@ findFirstUnique([4,5,1,2,0,4]) ---> 5
 findFirstUnique([9,2,3,2,6,6]) ---> 9
 
  */
+
+function findFirstUnique(arr) {
+  let j;
+
+  for (let i = 0; i < arr.length; i++) {
+    j = 0;
+
+    while (j < arr.length) {
+      if (i != j && arr[i] == arr[j]) break;
+      j += 1;
+    }
+    if (j == arr.length) return arr[i];
+  }
+
+  return null;
+}
+
+/**
+ * We start from the first element and traverse through the whole array comparing it with all the other elements to see if any element is equal to it.
+ * If so, we skip to the next element and repeat the same steps.
+ * If not, then this is the first unique element in the array, like 0 is in the given example.
+ */
