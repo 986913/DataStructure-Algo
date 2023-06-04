74175e3a1c9b15176c2c94400e398685d71ba1e0
Sun flower <MLIU2@LTU.EDU>
2023-06-04 16:45:47 -0400

    changea bit

--- Algorithm/2 Pointers/removeDuplicates.lc26.js
+++ Algorithm/2 Pointers/removeDuplicates.lc26.js
@@ -7,14 +7,14 @@
 var removeDuplicates = function (nums) {
   if (nums == null) return 0;
 
-  let slow = 1;
-
+  let slow = 0;
   for (let fast = 1; fast < nums.length; fast++) {
-    if (nums[fast] !== nums[slow - 1]) {
-      nums[slow] = nums[fast];
+    if (nums[slow] !== nums[fast]) {
       slow++;
+      nums[slow] = nums[fast];
     }
   }
 
-  return slow;
+  return slow + 1; //这个返回的是长度
+  // arr.splice(slow + 1) //这个返回的是in place去重后的数组
 };
