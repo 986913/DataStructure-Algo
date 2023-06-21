112eef29b9040b5d755b3f1b5d51fffb14f8c610
Sun flower <MLIU2@LTU.EDU>
2023-06-21 17:15:52 -0400

    update math.pow

--- Algorithm/Recursion/power.js
+++ Algorithm/Recursion/power.js
@@ -1,17 +1,17 @@
 /*
-  write a function called power, which accepts a base and exponent,
+  write a function called pow, which accepts a base and exponent,
   the function should return the power of the base to the exponent,
   this function should mimic the Math.pow()
-
-
   eg： 
-      power(2,0)   //1
-      power(2,1)   //2
-      power(2,2)   //4
-      power(2,3)   //8
+      pow(2,0)   //1
+      pow(2,1)   //2
+      pow(2,2)   //4
+      pow(2,3)   //8
+      pow(4, -1)
  */
 
-function power(base, exponent){
-  if(exponent === 0) return 1;
-  return base * power(base, exponent-1)
+function pow(base, exponent) {
+  if (exponent < 0) return 1 / pow(base, -exponent);
+  if (exponent === 0) return 1;
+  return base * pow(base, exponent - 1);
 }
