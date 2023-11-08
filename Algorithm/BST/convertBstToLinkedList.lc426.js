c671bc7bd0b0d318e13dfb6695fe52532f9a3d7e
Sun flower <MLIU2@LTU.EDU>
2023-11-07 20:14:18 -0500

    bst to list

---
+++ Algorithm/BST/convertBstToLinkedList.lc426.js
@@ -0,0 +1,53 @@
+/**
+ // Definition for a Node.
+ class Node {
+     public int val;
+     public Node left;
+     public Node right;
+    
+     public Node() {}
+    
+     public Node(int _val) {
+        val = _val;
+     }
+    
+     public Node(int _val,Node _left,Node _right) {
+         val = _val;
+         left = _left;
+         right = _right;
+     }
+ };
+ */
+
+const convertToLinkedList = (root) => {
+  let pre = null;
+  let head = null;
+
+  const helper = (node) => {
+    if (!node) return;
+
+    if (node.left) helper(node.left); // 左
+
+    /* 中 */
+    if (head == null) {
+      // first node
+      head = new Node();
+      head.right = node;
+    } else {
+      node.left = pre;
+      pre.right = node;
+      node.left = pre;
+    }
+    pre = node;
+
+    if (node.right) helper(node.right); // 右
+  };
+
+  helper(root);
+  head.right.left = pre;
+  pre.right = head.right;
+
+  return head.right;
+};
+
+// https://www.jiakaobo.com/leetcode/426.%20Convert%20Binary%20Search%20Tree%20to%20Sorted%20Doubly%20Linked%20List.htmls
