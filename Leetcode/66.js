e6740b0ce2395b55c982c9713e3166ea92b950c0
Sun flower <MLIU2@LTU.EDU>
2023-11-11 12:35:04 -0500

    add comments

--- Leetcode/66.js
+++ Leetcode/66.js
@@ -4,6 +4,7 @@
  */
 
 var plusOne = function (digits) {
+  // for common case, like: [1,2] [1,2,9]
   for (let i = digits.length - 1; i >= 0; i--) {
     if (digits[i] < 9) {
       digits[i] += 1;
@@ -12,6 +13,6 @@ var plusOne = function (digits) {
       digits[i] = 0;
     }
   }
-  // make [9,9,9], [1,0,0,0] not [0,0,0]
+  // for special case: [9] [9,9,9] etc. (make [9,9,9], [1,0,0,0] not [0,0,0])
   return [1, ...digits];
 };
