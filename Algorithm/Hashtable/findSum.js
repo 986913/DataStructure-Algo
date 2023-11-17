c6b9918f356f9ee0992d6fc1376f87bf7a68febe
Sun flower <MLIU2@LTU.EDU>
2023-11-17 16:58:50 -0500

    findsum

---
+++ Algorithm/Hashtable/findSum.js
@@ -0,0 +1,9 @@
+/**
+  implement the findSum(arr, value) function, which takes an array arr, a number and value as input 
+  and returns an array of two numbers that add up to value.
+ */
+
+/* --------------- 用例测试 --------------- */
+findSum([1, 21, 3, 14, 5, 60, 7, 6], 81); // [21,60]
+findSum([1, 2, 3, 4], 10); // false
+findSum([1, 2, 3], 5); // [2,3]
