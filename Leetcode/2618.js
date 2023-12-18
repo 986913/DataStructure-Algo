6d45dcf05e6c0a0d39238e0eb717e7f722a81f2f
Sun flower <MLIU2@LTU.EDU>
2023-12-17 19:21:53 -0500

    add new

---
+++ Leetcode/2618.js
@@ -0,0 +1,20 @@
+/**
+ * @param {*} obj
+ * @param {*} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function (obj, classFunction) {
+  if (obj === null || obj === undefined || typeof classFunction !== 'function')
+    return false;
+
+  while (obj !== null) {
+    if (obj.__proto__ == classFunction.prototype) return true;
+    obj = obj.__proto__;
+  }
+
+  return false;
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
