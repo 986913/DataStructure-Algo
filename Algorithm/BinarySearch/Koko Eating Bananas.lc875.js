b5d90de47336b542bac006860cb1ced26c121b98
Sun flower <MLIU2@LTU.EDU>
2024-01-08 09:58:45 -0500

    koko

---
+++ Algorithm/BinarySearch/Koko Eating Bananas.lc875.js
@@ -0,0 +1,44 @@
+/* ------------------Solution1: 线性扫描 ------------------------------------------------------- */
+const canFinish = (piles, speed, h) => {
+  let time = 0;
+  piles.forEach((n) => {
+    //Math.ceil(n / speed) ----> 以speed的速度吃n个香蕉，需要的时间：
+    time += Math.ceil(n / speed);
+  });
+  return time <= h;
+};
+
+var minEatingSpeed = function (piles, h) {
+  const max = Math.max(...piles);
+  //线性扫描：
+  for (let speed = 0; speed < max; speed++) {
+    if (canFinish(piles, speed, h)) return speed;
+  }
+  return max;
+};
+
+/* ------------------Solution2(Sol1的优化版)：2分扫描 ------------------------------------------------------- */
+const canFinish = (piles, speed, h) => {
+  let time = 0;
+  piles.forEach((n) => {
+    //Math.ceil(n / speed) ----> 以speed的速度吃n个香蕉，需要的时间：
+    time += Math.ceil(n / speed);
+  });
+  return time <= h;
+};
+
+var minEatingSpeed = function (piles, h) {
+  const max = Math.max.apply(Math, piles);
+  let left = 1;
+  let right = max;
+  //2分扫描：
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (canFinish(piles, mid, h)) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left;
+};
