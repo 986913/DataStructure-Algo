23526a2aa0062dbca1ce1452fff97088af6f61c4
Sun flower <MLIU2@LTU.EDU>
2024-01-27 14:43:34 -0500

    845

--- Algorithm/Dynamic Programming/Longest Mountain in Array.lc845.js
+++ Algorithm/Dynamic Programming/Longest Mountain in Array.lc845.js
@@ -1,55 +1,25 @@
-/*******************************Solution 1: DP ****************************************/
-class Solution {
-    public int longestMountain(int[] arr) {
-        int res = 0, up = 0, down = 0, n = arr.length;
+/******************************* Solution 1: DP / Two-pointer ****************************************/
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var longestMountain = function (arr) {
+  let result = 0;
 
-        for (int i = 1; i < n; i++) {
-            // 从递减变成递增,重新开始计数
-            if ((down != 0 && arr[i] > arr[i - 1]) || (arr[i - 1] == arr[i])) {
-                up = down = 0;
-            }
+  // 用于跟踪递增和递减序列的长度
+  let up = 0;
+  let down = 0;
 
-            // 递增数列
-            if (arr[i] > arr[i - 1]) {
-                up++;
-            }
-            // 递减数列
-            if (arr[i] < arr[i - 1]) {
-                down++;
-            }
-
-            if (up > 0 && down > 0) {
-                res = Math.max(res, up + down + 1);
-            }
-        }
-
-        return res;
+  /* 从索引1开始遍历数组, pointer1是i-1，pointer2是i: */
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i - 1] === arr[i] || (down != 0 && arr[i - 1] < arr[i])) {
+      //如果存在高原（相等的元素）OR 从递减变成递增，那就重新计数
+      up = down = 0;
     }
-}
+    if (arr[i - 1] < arr[i]) up++; //递增数列
+    if (arr[i - 1] > arr[i]) down++; //递减数列
+    if (up > 0 && down > 0) result = Math.max(result, up + down + 1); //重点在这： 如果同时存在递增和递减序列，则更新结果
+  }
 
-/*******************************Solution 2: Two pointer ****************************************/
-class Solution {
-    public int longestMountain(int[] arr) {
-        int[] up = new int[arr.length];
-        int[] down = new int[arr.length];
-
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > arr[i - 1]) {
-                up[i] = up[i - 1] + 1;
-            }
-        }
-
-        int res = 0;
-        for (int i = arr.length - 2; i >= 0; i--) {
-            if (arr[i] > arr[i + 1]) {
-                down[i] = down[i + 1] + 1;
-            }
-
-            if (up[i] != 0 && down[i] != 0) {
-                res = Math.max(res, up[i] + down[i] + 1);
-            }
-        }
-
-        return res;
-    }
-}
+  return result;
+};
