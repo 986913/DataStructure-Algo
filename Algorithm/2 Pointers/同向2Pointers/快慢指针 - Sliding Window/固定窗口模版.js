2e00cdb10c79cc94c065973c11d197165232ce2c
Sun flower <MLIU2@LTU.EDU>
2024-02-16 14:45:54 -0500

    add new sliding window

--- Algorithm/2 Pointers/同向2Pointers/快慢指针 - Sliding Window/固定窗口模版.js
+++ Algorithm/2 Pointers/同向2Pointers/快慢指针 - Sliding Window/固定窗口模版.js
@@ -1,19 +1,22 @@
 /******************************参照LC   28题************************************* */
 /******************************参照LC  239题************************************* */
+/******************************参照LC  904题************************************* */
 
-const fixedSlidingWindow = (target, nums) => {
+const fixedSlidingWindow = (target, s) => {
+  // 用合适的数据结构记录窗口中的数据
+  const window = new Map();
+
   let slow = 0;
   let fast = 0;
+  while (fast < s.length) {
+    let moveIn = s[fast]; // moveIn 是将移入窗口的字符
+    window.set(moveIn, window.get(moveIn) + 1 || 1); // 进行窗口内数据的一系列更新。。。
+    fast++; //增大窗口
 
-  while (fast < nums.length) {
-    //增大窗口
-    window.add(nums[fast]);
-    fast++;
-
-    if (something.length >= windowSize) {
-      //缩小窗口
-      window.remove(nums[slow]);
-      slow++;
+    if (window.length >= fixedSize) {
+      let moveOut = s[slow]; // moveOut 是将移出窗口的字符
+      window.set(moveOut, window.get(moveOut) - 1); // 进行窗口内数据的一系列更新。。。
+      slow++; // 缩小窗口
     }
   }
 };
