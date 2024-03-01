38afaddb22a7a6bd88a8a355dfca47496fabd7a8
Sun flower <MLIU2@LTU.EDU>
2024-03-01 15:12:48 -0500

    add comments

--- Algorithm/2 Pointers/相向2Pointers/常见相向2Pointer/Reverse String II.lc541.js
+++ Algorithm/2 Pointers/相向2Pointers/常见相向2Pointer/Reverse String II.lc541.js
@@ -4,14 +4,15 @@
  * @return {string}
  */
 
+/*******************************  👍👍👍 2 pointer *************************************/
 function reverseStr(s, k) {
   const arr = s.split('');
 
-  // 每计数至 2k 个字符 -> foreach use 2K as step
+  //i 每次移动 2 * k
   for (let i = 0; i < arr.length; i += 2 * k) {
     let left = i;
-    let right = i + k - 1; //就反转这 2k 个字符中的前 k 个字符 --> 就确定了right的开始位置(相对于left+k的位置)
-
+    let right = i + k - 1; //就反转这2k个字符中的前k个字符 --> 就确定了right的开始位置(相对于left+k的位置)
+    // 每隔2k个字符的前k个字符进行反转;
     while (left < right) {
       [arr[left], arr[right]] = [arr[right], arr[left]];
       left++;
--- Leetcode/541.js
+++ Leetcode/541.js
@@ -4,10 +4,11 @@
  * @return {string}
  */
 
+/*******************************  👍👍👍 2 pointer *************************************/
 function reverseStr(s, k) {
   const arr = s.split('');
 
-  // 每计数至 2k 个字符 -> foreach use 2K as step
+  // 每计数至2k个字符 -> foreach use 2K as step
   for (let i = 0; i < arr.length; i += 2 * k) {
     let left = i;
     let right = i + k - 1; //就反转这 2k 个字符中的前 k 个字符 --> 就确定了right的开始位置(相对于left+k的位置)
