7e350597b96ea2d40e3a5d487479391607c43157
Sun flower <MLIU2@LTU.EDU>
2024-03-06 13:59:10 -0500

    inorder

--- Algorithm/BinaryTree二叉树/二叉树遍历/DFS/inorderTraversal.lc94.js
+++ Algorithm/BinaryTree二叉树/二叉树遍历/DFS/inorderTraversal.lc94.js
@@ -31,22 +31,24 @@ const inorderTraversal = (root) => {
 };
 
 /*************************** Solution 2: 👍👍👍 Iteration 迭代  ----------------------------------------------- */
+// https://www.bilibili.com/video/BV1Zf4y1a77g/?spm_id_from=333.788&vd_source=8b5297d974f6a5e72c60ec8ea33f2ff6
 const inorderTraversal = (root) => {
   let cur = root; //指针用来访问节点
 
   const stack = [];
   let visited = [];
 
-  while (stack.length || cur) {
-    //指针用来访问节点,访问到最底层
-    if (cur) {
-      stack.push(cur); // 将访问的节点放进栈
-      cur = cur.left; // 左
+  while (stack.length || cur !== null) {
+    // 当指针cur不为空的时候，要入栈:
+    if (cur !== null) {
+      stack.push(cur); // 入栈
+      cur = cur.left; // 指针用来访问节点,访问到最底层 (一路向左)
     } else {
-      cur = stack.pop(); // --> 弹出 中
+      // 当指针cur为空的时候，要出栈:
+      cur = stack.pop();
       visited.push(cur.val);
 
-      cur = cur.right; // 右
+      cur = cur.right; // 更新cur
     }
   }
 
--- Algorithm/BinaryTree二叉树/二叉树遍历/DFS/模版/迭代/DFS_迭代_postOrder(模版).js
+++ Algorithm/BinaryTree二叉树/二叉树遍历/DFS/模版/迭代/DFS_迭代_postOrder(模版).js
@@ -9,10 +9,10 @@ const postorderTraversal = (root) => {
     const curr = stack.pop();
     visited.push(curr.val); // update result before 入栈
 
-    // 入栈： 先左 -> 后右 (相对于前序遍历，这更改一下入栈顺序)s
+    // 入栈： 先左 -> 后右 (相对于前序遍历，这更改一下入栈顺序)
     if (curr.left) stack.push(curr.left);
     if (curr.right) stack.push(curr.right);
   }
 
-  return visited.reverse(); //  中 -> 右 -> 左  反转为 ： 左 -> 右 -> 中
+  return visited.reverse(); //  中 -> 右 -> 左  反转为 ： 左 -> 右 -> 中  (相对于前序遍历，这里reverse一下)
 };
