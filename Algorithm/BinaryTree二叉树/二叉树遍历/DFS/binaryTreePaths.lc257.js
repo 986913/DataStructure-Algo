018782bae731939f8912af34a229fc9baf8aa334
Sun flower <MLIU2@LTU.EDU>
2024-03-19 17:46:27 -0400

    add comments

--- Algorithm/BinaryTree二叉树/二叉树遍历/DFS/binaryTreePaths.lc257.js
+++ Algorithm/BinaryTree二叉树/二叉树遍历/DFS/binaryTreePaths.lc257.js
@@ -12,7 +12,7 @@
  */
 // https://www.bilibili.com/video/BV1ZG411G7Dh/?vd_source=2efba544aa6c1bd084ec6ddd7a98c6b2
 
-/***************** Solution1: 👍👍👍 DFS PreOrder + backtracking (LC144, 129变形题🟡) *****************s*/
+/***************** Solution1: 👍 DFS PreOrder + backtracking (LC144, 129变形题🟡) *****************s*/
 var binaryTreePaths = function (root) {
   const visited = [];
 
@@ -38,7 +38,7 @@ var binaryTreePaths = function (root) {
 
   //1. 确定递归函数参数:树的根节点 和 当前的每一条path:curPath为数组, 无返回值
   const helper = (node, curPath) => {
-    curPath.push(node.val); // 中，中为什么写在这里，因为最后一个节点也要加入到path中
+    curPath.push(node.val); // 中：中为什么写在这里，因为最后一个节点也要加入到path中
 
     //2. 确定终止条件，到叶子节点就终止, 更新result;
     if (!node.left && !node.right) {
@@ -46,15 +46,16 @@ var binaryTreePaths = function (root) {
       return;
     }
 
+    // curPath.push(node.val); // 🚫中不能写在这里,写在这里会落下叶子结点的val, 因为上面终止条件直接就return了
     //3. 确定单层递归逻辑
     if (node.left) {
       //左
-      helper(node.left, curPath);
+      helper(node.left, curPath); // <-- 递归
       curPath.pop(); // <-- backtracking
     }
     if (node.right) {
       //右
-      helper(node.right, curPath);
+      helper(node.right, curPath); // <-- 递归
       curPath.pop(); // <-- backtracking
     }
   };
