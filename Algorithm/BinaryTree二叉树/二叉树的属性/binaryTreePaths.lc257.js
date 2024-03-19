fbc5e6d06fd52752ffdac3f11207307f94cc4884
Sun flower <MLIU2@LTU.EDU>
2024-03-19 15:41:14 -0400

    257

--- Algorithm/BinaryTree二叉树/二叉树的属性/binaryTreePaths.lc257.js
+++ Algorithm/BinaryTree二叉树/二叉树的属性/binaryTreePaths.lc257.js
@@ -10,14 +10,35 @@
  * @param {TreeNode} root
  * @return {string[]}
  */
+// https://www.bilibili.com/video/BV1ZG411G7Dh/?vd_source=2efba544aa6c1bd084ec6ddd7a98c6b2
 
-/***************** Solution: 👍👍👍 DFS PreOrder + backtracking (LC144, 129变形题🟡) *****************s*/
+/***************** Solution1: 👍👍👍 DFS PreOrder + backtracking (LC144, 129变形题🟡) *****************s*/
+var binaryTreePaths = function (root) {
+  const visited = [];
+
+  const helper = (node, curPathString) => {
+    if (!node.left && !node.right) {
+      curPathString += node.val;
+      visited.push(curPathString);
+      return;
+    }
+
+    curPathString = curPathString + node.val + '->'; //中
+    if (node.left) helper(node.left, curPathString); //左
+    if (node.right) helper(node.right, curPathString); //右
+  };
+
+  helper(root, '');
+  return visited;
+};
+
+/***************** Solution2: 👍👍👍 DFS PreOrder + backtracking (LC144, 129变形题🟡) *****************s*/
 var binaryTreePaths = function (root) {
   const result = [];
 
   //1. 确定递归函数参数:树的根节点 和 当前的每一条path:curPath为数组, 无返回值
   const helper = (node, curPath) => {
-    curPath.push(node.val); // 中
+    curPath.push(node.val); // 中，中为什么写在这里，因为最后一个节点也要加入到path中
 
     //2. 确定终止条件，到叶子节点就终止, 更新result;
     if (!node.left && !node.right) {
@@ -41,5 +62,3 @@ var binaryTreePaths = function (root) {
   helper(root, []); // initial curpath is []
   return result;
 };
-
-//https://www.bilibili.com/video/BV1ZG411G7Dh/?vd_source=2efba544aa6c1bd084ec6ddd7a98c6b2
