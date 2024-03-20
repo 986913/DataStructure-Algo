d8845bbe443f34c22c37c9cd4761c6d93491ebd2
Sun flower <MLIU2@LTU.EDU>
2024-03-20 16:32:27 -0400

    105

--- Algorithm/BinaryTree二叉树/二叉树遍历/DFS/buildBinaryTreeFromPreorderAndInorder.lc105.js
+++ Algorithm/BinaryTree二叉树/二叉树遍历/DFS/buildBinaryTreeFromPreorderAndInorder.lc105.js
@@ -12,19 +12,56 @@
  * @return {TreeNode}
  */
 
-// 👍 https://www.bilibili.com/video/BV1vW4y1i7dn/?vd_source=2efba544aa6c1bd084ec6ddd7a98c6b2
-/****************  DFS - Preorder 🟡 lc106变形题*****************************************/
+/************************************  DFS PreOrder 🟡LC 106变形题 ****************************************
+  👍 https://www.bilibili.com/video/BV1vW4y1i7dn/?vd_source=2efba544aa6c1bd084ec6ddd7a98c6b2
+  
+  核心思想：先构造中间节点，然后递归构造左子树和右子树
+  Steps:
+    1. pre order数组length为0时, 代表没有中节点，返回null;
+    2. pre order数组第一个元素就是中节点(root)
+    3. 寻找in order数组中的root元素的位置(rootIndex)作为切割点
+    4. 先切割inorder
+    5. 后切割preorder
+    6. 递归处理左区间右区间
+
+                    inorder: 3  9  20  15  7
+                   preorder: 3️⃣  3  15  20  7
+                            / \
+                          /    \
+                  inorder: 9    inorder : 15  20  7
+                 preorder: 9️⃣   preorder: 2️⃣0️⃣ 15  7
+                                  /  \
+                                /     \
+                        inorder: 15   inorder:   7
+                      preorder: 1️⃣5️⃣  preorder:  7️⃣
+
+            ----------------------------------------------
+                          上面的流程会build tree:
+                                  3
+                                /   \
+                              9     20
+                                  /   \  
+                                15     7
+*/
+
 var buildTree = function (preorder, inorder) {
   if (!preorder.length) return null;
 
-  const rootVal = preorder.shift(); // 从前序遍历的数组中获取中间节点的值， 即数组第一个值
-  const root = new TreeNode(rootVal); // 创建中间节点
-  const index = inorder.indexOf(rootVal); // 获取中间节点在中序遍历中的下标
+  // 中
+  const rootVal = preorder.shift(); // <-- differe is here, preorder第s一个元素就是中间节点
+  const root = new TreeNode(rootVal);
+  const rootIndex = inorder.indexOf(rootVal);
 
-  //创建左节点
-  root.left = buildTree(preorder.slice(0, index), inorder.slice(0, index));
-  // 创建右节点
-  root.right = buildTree(preorder.slice(index), inorder.slice(index + 1)); // 注意inorder: rootIndex + 1 here
+  // 左： 创建左节点
+  root.left = buildTree(
+    preorder.slice(0, rootIndex),
+    inorder.slice(0, rootIndex)
+  );
+  // 右： 创建右节点
+  root.right = buildTree(
+    preorder.slice(rootIndex),
+    inorder.slice(rootIndex + 1) // 注意inorder: rootIndex + 1 here
+  );
 
   return root;
 };
