5e24f50f1e9ad4e68790b134f7684251e74d6fc2
Sun flower <MLIU2@LTU.EDU>
2024-03-20 21:37:04 -0400

    654

--- Algorithm/BinaryTree二叉树/二叉树遍历/DFS/buildMaximumBinaryTree.lc654.js
+++ Algorithm/BinaryTree二叉树/二叉树遍历/DFS/buildMaximumBinaryTree.lc654.js
@@ -12,13 +12,23 @@
  * @return {TreeNode}
  */
 
-/****************  DFS - Preorder 🟡 类似于lc106*****************************************/
+/****************  DFS - Preorder 🟡类似于LC 105, 106****************************************
+  👍 https://www.bilibili.com/video/BV1MG411G7ox/?spm_id_from=333.999.0.0&vd_source=8b5297d974f6a5e72c60ec8ea33f2ff6
+  
+  核心思想：先构造中间节点，然后递归构造左子树和右子树
+  Steps:
+    1. nums数组length为0时, 代表没有中节点，返回null;
+    2. nums中最大的数maxValue就是中节点，确定maxValue的下标(maxValueIdx)
+    3. 根据maxValueIdx切割nums为左区间 右区间
+    4. 递归构造左节点，右节点
+    5. return中节点
+*/
 
 // 递归参数就是传入的是存放元素的数组，返回该数组构造的二叉树的头结点：
 var constructMaximumBinaryTree = function (nums) {
-  if (nums.length === 0) return null;
+  if (!nums.length) return null;
 
-  /*构造中间节点*/
+  //得出nums中的最大值和其下标
   let maxValue = -Infinity;
   let maxValueIdx = -1;
   for (let index = 0; index < nums.length; index++) {
@@ -27,8 +37,9 @@ var constructMaximumBinaryTree = function (nums) {
       maxValueIdx = index;
     }
   }
-  let rootNode = new TreeNode(maxValue);
 
+  /*构造中间节点*/
+  let rootNode = new TreeNode(maxValue);
   /*递归构造左子树：*/
   rootNode.left = constructMaximumBinaryTree(nums.slice(0, maxValueIdx));
   /*递归构造右子树：*/
