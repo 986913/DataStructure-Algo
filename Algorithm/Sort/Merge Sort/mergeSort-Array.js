7cc9b5374b605096df99ee2095acf68eee87595a
Sun flower <MLIU2@LTU.EDU>
2024-03-21 20:43:50 -0400

    remove

--- Algorithm/Sort/Merge Sort/mergeSort-Array.js
+++ Algorithm/Sort/Merge Sort/mergeSort-Array.js
@@ -13,43 +13,6 @@
     space complexity         O(n)
 */
 
-/* ---------------------------------------- Solution: (Not in-place) ------------------------------------------------------ */
-// Main function:
-function mergeSort(arr) {
-  if (arr.length <= 1) return arr;
-
-  let mid = Math.floor(arr.length / 2);
-  let left = mergeSort(arr.slice(0, mid));
-  let right = mergeSort(arr.slice(mid));
-
-  return merge(left, right); // helper function
-}
-// helper function: merges two sorted arrays (use 2 pointer)
-function merge(arr1, arr2) {
-  let results = [];
-  let i = 0;
-  let j = 0;
-
-  while (i < arr1.length && j < arr2.length) {
-    if (arr2[j] > arr1[i]) {
-      results.push(arr1[i]);
-      i++;
-    } else {
-      results.push(arr2[j]);
-      j++;
-    }
-  }
-  while (i < arr1.length) {
-    results.push(arr1[i]);
-    i++;
-  }
-  while (j < arr2.length) {
-    results.push(arr2[j]);
-    j++;
-  }
-  return results;
-}
-
 /* ---------------------------------------- Solution: (In-place) ------------------------------------------------------ */
 function mergeSort(arr) {
   if (arr.length <= 1) return arr;
