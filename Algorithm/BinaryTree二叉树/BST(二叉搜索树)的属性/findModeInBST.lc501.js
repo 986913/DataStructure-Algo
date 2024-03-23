49fc3a3b7f570f60e86395052f669a199f80021a
Sun flower <MLIU2@LTU.EDU>
2024-03-23 11:46:09 -0400

    501

--- Algorithm/BinaryTree二叉树/BST(二叉搜索树)的属性/findModeInBST.lc501.js
+++ Algorithm/BinaryTree二叉树/BST(二叉搜索树)的属性/findModeInBST.lc501.js
@@ -9,9 +9,11 @@
 /**
  * @param {TreeNode} root
  * @return {number[]}
+ * https://www.bilibili.com/video/BV1fD4y117gp/?spm_id_from=333.788&vd_source=8b5297d974f6a5e72c60ec8ea33f2ff6
  */
 
-/**************** 👍 Solution 1: dfs inorder to create visited map, then find the most high frequency of that map  🟡lc530变形题 ***********/
+/**************** Solution 1: DFS Inorder - Recursion, use map assistance LC530变形题 ***********/
+
 var findMode = function (root) {
   const map = new Map();
   let result = [];
@@ -39,18 +41,18 @@ var findMode = function (root) {
   return result;
 };
 
-/*-------------- 👍👍 Solution 2: dfs inorder, but no need to create map! 🟡lc530变形题----------------------*/
+/**************** 👍 Solution 2: DFS Inorder - Recursion, No need map assistance 🟡LC530变形题 ***********/
 var findMode = function (root) {
   let result = [];
-  let pre = null;
-  let count = 0; // 记录单个元素出现的频率
-  let maxCount = 1; // 记录整个树中某元素出现的最大频率
+  let pre = null; //<--- pre记录前一个节点
+  let count = 0; //<---- 记录单个元素出现的频率
+  let maxCount = 1; //<--记录整个树中某元素出现的最大频率
 
   const helper = (node) => {
     if (!node) return;
 
     helper(node.left);
-
+    /************ 中序位置 *****************/
     if (!pre) count = 1; // 第一个节点
     else if (pre && node.val === pre.val) count++; // 与前一个节点数值相同
     else count = 1; // 与前一个节点数值不同
@@ -66,10 +68,52 @@ var findMode = function (root) {
       result = [];
       result.push(node.val);
     }
-
+    /************************************/
     helper(node.right);
   };
 
   helper(root);
+  return result;
+};
+
+/************** Solution 3: DFS Inorder - Iteration LC94模版变形题，LC530 ****************/
+var findMode = function (root) {
+  let cur = root;
+
+  const stack = [];
+  let result = [];
+  let pre = null;
+  let count = 0;
+  let maxCount = 1;
+
+  while (stack.length || cur) {
+    if (cur) {
+      stack.push(cur);
+      cur = cur.left;
+    } else {
+      cur = stack.pop();
+
+      /*******************中**********************/
+      if (!pre) count = 1; // 第一个节点
+      else if (pre && cur.val === pre.val) count++; // 与前一个节点数值相同
+      else count = 1; // 与前一个节点数值不同
+
+      pre = cur; // 更新上一个节点
+
+      if (count === maxCount) {
+        // 如果和最大值相同，放进result中
+        result.push(cur.val);
+      } else if (count > maxCount) {
+        // 如果计数大于最大值频率，要更新最大频率，要清空result，因为之前result里的元素都失效了， 最后更新result
+        maxCount = count;
+        result = [];
+        result.push(cur.val);
+      }
+      /******************************************/
+
+      cur = cur.right;
+    }
+  }
+
   return result;
 };
