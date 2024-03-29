3d483c1f4512e8f0fcaff2970ac25170f8fa03b7
Sun flower <MLIU2@LTU.EDU>
2024-03-29 15:32:00 -0400

    lc100

--- Algorithm/BinaryTree二叉树/二叉树遍历/DFS/isSameTree.lc100.js
+++ Algorithm/BinaryTree二叉树/二叉树遍历/DFS/isSameTree.lc100.js
@@ -12,15 +12,15 @@
  * @return {boolean}
  */
 
-/* --------- 👍👍👍 DFS post_order - recursion-----------------------------*/
+/********************* Solution1: 👍👍👍 DFS PostOrder - Recursion 分解思想 *********************/
 
 // 1. 确定递归的参数:两个tree NODE : p and q.  和返回值boolean: true false
 var isSameTree = function (p, q) {
   //2. 确定终止条件 空的情况
   if (p === null && q !== null) return false;
-  else if (p !== null && q === null) return false;
-  else if (p === null && q === null) return true;
-  else if (p.val !== q.val) return false;
+  if (p !== null && q === null) return false;
+  if (p === null && q === null) return true;
+  if (p.val !== q.val) return false;
 
   //3. 当p.val===q.val, 确定单层递归逻辑:
   let isLeftSideSame = isSameTree(p.left, q.left);
