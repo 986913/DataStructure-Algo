3026af8a72a5a26e7d45a351af31286c24634aff
Sun flower <MLIU2@LTU.EDU>
2024-04-05 11:12:40 -0400

    add new

---
+++ Algorithm/Graph/distanceK.lc863.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} target
+ * @param {number} k
+ * @return {number[]}
+ */
+
+/* ------------------ solution1: 👍👍 BFS遍历 ------------------*/
+var distanceK = function (root, target, k) {};
+
+/* ------------------ solution2: DFS 遍历思想 ------------------*/
