73ddeb0d17f4308cbc12f610d09b99a78cc9cccd
Sun flower <MLIU2@LTU.EDU>
2024-05-06 13:46:31 -0400

    96 lvc

--- Leetcode/343.js
+++ Leetcode/343.js
@@ -1,32 +1,74 @@
 /**
-1. 确定dp数组以及下标的含义: 分拆数字i，可以得到的最大乘积为dp[i]
-2. 确定递推公式: 状态转移方程 dp[i] = max( dp[i], (i-j)*j, dp[i-j]*j );         
-                理解： j * (i - j) 是单纯的把整数拆分为两个数相乘，而j * dp[i - j]是拆分成两个以及两个以上的个数相乘。
-3. dp数组如何初始化: dp[0]=0, dp[1]=0，dp[2]= 1 （从dp[i]的定义来说，拆分数字2，得到的最大乘积是1）
-4. 确定遍历顺序: 
-    for (int i = 3; i <= n ; i++) {
-      for (int j = 1; j <= i / 2; j++) {
-        dp[i] = max(dp[i], max((i - j) * j, dp[i - j] * j));
-      }
-    }
-5. 举例推导dp数组: 
-      举例当n为10 的时候，dp数组里的数值: [ 0,0,1,2,4,6,9,12,18,27,36 ]
- */
-
-/**
  * @param {number} n
  * @return {number}
  */
-/* -------------------- solution:  DP  ------------------  */
+
+/********************************* Solution 1:  Top to Bottom 递归 - O(n^2) ****************************
+  递归时间复杂度分析：递归函数调用的次数 * 递归函数本身的复杂度
+                       O(n) * O(n)
+*/
 var integerBreak = function (n) {
-  let dp = new Array(n + 1).fill(0);
-  dp[2] = 1;
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  let maxProduct = -Infinity;
+  for (let i = 1; i < n; i++) {
+    // 将 n 拆分成 i 和 n-i，计算乘积，并递归求解
+    const currProduct = i * Math.max(n - i, integerBreak(n - i));
+    maxProduct = Math.max(maxProduct, currProduct);
+  }
+  return maxProduct;
+};
+
+/********************************* Solution 2:  Top to Bottom 递归 (带备忘录) - O(n^2) ****************************
+  递归时间复杂度分析：递归函数调用的次数 * 递归函数本身的复杂度
+                       O(n) * O(n)
+*/
+var integerBreak = function (n) {
+  let memo = new Array(n + 1).fill(-1);
+  return helper(n, memo);
+};
+
+const helper = (n, memo) => {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  // 准备递归之前，先去备忘录里查一下，算过了就不用再计算了， 没算就准备递归计算
+  if (memo[n] !== -1) return memo[n];
+
+  let maxProduct = -Infinity;
+  for (let i = 1; i < n; i++) {
+    // 将 n 拆分成 i 和 n-i，计算乘积，并递归求解
+    const currProduct = i * Math.max(n - i, integerBreak(n - i));
+    maxProduct = Math.max(maxProduct, currProduct);
+  }
+  memo[n] = maxProduct; //把递归结果存到备忘录里
+  return maxProduct;
+};
+
+/********************************** Solution 3: 自底向上 迭代DP - O(n^2) *************************************  
+
+  1. 确定dp数组以及下标的含义:      分拆数字i，可以得到的最大乘积为dp[i]
+  2. 确定递推公式状态转移方程       dp[i] = max( dp[i], j*(i-j), j*dp[i-j] );         
+                                理解： j*(i-j)是单纯的把整数拆分为两个数相乘，而j*dp[i-j]是拆分成两个以及两个以上的个数相乘。
+  3. dp数组如何初始化:            dp[0]=0, dp[1]=0，dp[2]= 1 （从dp[i]的定义来说，拆分数字2，得到的最大乘积是1）
+  4. 确定遍历顺序: 
+                                for (int i = 3; i <= n ; i++) {
+                                  for (int j = 1; j < i; j++) {
+                                    dp[i] = Math.max(dp[i], j*(i-j), j*dp[i-j])
+                                  }
+                                }
+  5. 举例推导dp数组:              举例当n为10 的时候，dp数组里的数值: [ 0,0,1,2,4,6,9,12,18,27,36 ]
+ */
+var integerBreak = function (n) {
+  let dp = new Array(n + 1).fill(-1);
+  dp[2] = 1; // 初始化dp[2]为1: 拆分数字2,得到的最大乘积是1
 
   for (let i = 3; i <= n; i++) {
-    for (let j = 1; j <= i / 2; j++) {
-      dp[i] = Math.max(dp[i], dp[i - j] * j, (i - j) * j);
+    // 循环条件应该是 i <= n
+    for (let j = 1; j < i; j++) {
+      dp[i] = Math.max(dp[i], j * (i - j), j * dp[i - j]); // 动态规划状态转移方程
     }
   }
-  // console.log(dp);
   return dp[n];
 };
--- 数据结构/BinaryTree二叉树/BST(二叉搜索树)的修改和构造/Unique Binary Search Trees I.lc96.js
+++ 数据结构/BinaryTree二叉树/BST(二叉搜索树)的修改和构造/Unique Binary Search Trees I.lc96.js
@@ -1,64 +1,75 @@
 /**
- * @param {number} n
+ * @param {number} n --> 代表个数！！！
  * @return {number}
  */
 
-/*********************************** Solution1:  DP  ********************************************
-1. 确定dp数组以及下标的含义: dp[i] ： 1到i为节点组成的二叉搜索树的个数为dp[i]。
-2. 确定递推公式: dp[i] += dp[j - 1] * dp[i - j]; ，      j-1 为j为头结点左子树节点数量，i-j 为以j为头结点右子树节点数量。
-3. dp数组如何初始化: dp[0]=1, dp[1]=1
-4. 确定遍历顺序: 
-   首先一定是遍历节点数，从递归公式：dp[i] += dp[j - 1] * dp[i - j]可以看出，节点数为i的状态是依靠 i之前节点数的状态。那么遍历i里面每一个数作为头结点的状态，用j来遍历。
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= i; j++) {
-          dp[i] += dp[j - 1] * dp[i - j];
-      }
-    }
-5. 举例推导dp数组: 举例当n为5 的时候，dp数组里的数值: [ 1,1,2,5,14,42 ]
- */
+/********************************* Solution 1:  Top to Bottom 递归 - O(n^2)  ****************************
+  递归时间复杂度分析：递归函数调用的次数 * 递归函数本身的复杂度
+  n为树的高度(input n) -> O(2^log(n)) * O(n)
+                      =       O(n) * O(n)
+*/
+var numTrees = function (n) {
+  if (n === 0 || n === 1) return 1;
 
-const numTrees = (n) => {
+  let counts = 0;
+  for (let i = 1; i <= n; i++) {
+    // i的值作为跟节点
+    const leftCounts = numTrees(i - 1);
+    const rightCounts = numTrees(n - i);
+    counts += leftCounts * rightCounts; // 左右子树的组合数乘积是BST的总数
+  }
+  return counts;
+};
+
+/********************************* Solution 2:  Top to Bottom 递归 (带备忘录) - O(n^2） ****************************
+  递归时间复杂度分析：递归函数调用的次数 * 递归函数本身的复杂度
+                       O(n) * O(n)
+*/
+var numTrees = function (n) {
+  // 生成n+1长度的备忘录
+  let memo = new Array(n + 1).fill(0);
+  return counts(n, memo);
+};
+
+const counts = (n, memo) => {
+  if (n === 0 || n === 1) return 1;
+
+  // 准备递归之前，先去备忘录里查一下，算过了就不用再计算了， 没算就准备递归计算
+  if (memo[n] !== 0) return memo[n];
+
+  let counts = 0;
+  for (let i = 1; i <= n; i++) {
+    const leftCounts = numTrees(i - 1, memo);
+    const rightCounts = numTrees(n - i, memo);
+    counts += leftCounts * rightCounts;
+  }
+  memo[n] = counts; // 把递归结果存到备忘录里
+  return memo[n];
+};
+
+/********************************** Solution 3: 自底向上 迭代DP  *************************************  
+
+1. 确定dp数组以及下标的含义:    dp[i]代表 1到i为节点组成的BST个数为dp[i]
+2. 确定递推公式:              dp[i] += dp[j - 1] * dp[i - j];       j-1为j为头结点左子树节点数量，i-j为以j为头结点右子树节点数量。
+3. dp数组如何初始化:          dp[0]=1, dp[1]=1
+4. 确定遍历顺序: 
+                            首先一定是遍历节点数，从递归公式：dp[i] += dp[j - 1] * dp[i - j]可以看出，节点数为i的状态是依靠 i之前节点数的状态。那么遍历i里面每一个数作为头结点的状态，用j来遍历。
+                              for (int i = 2; i <= n; i++) {
+                                for (int j = 1; j <= i; j++) {
+                                    dp[i] += dp[j - 1] * dp[i - j];
+                                }
+                              }
+5. 举例推导dp数组:            举例当n为5 的时候，dp数组里的数值: [ 1,1,2,5,14,42 ]
+ */
+var numTrees = function (n) {
   let dp = new Array(n + 1).fill(0);
   dp[0] = 1;
   dp[1] = 1;
 
   for (let i = 2; i <= n; i++) {
     for (let j = 1; j <= i; j++) {
-      dp[i] += dp[j - 1] * dp[i - j];
+      dp[i] = dp[i] + dp[j - 1] * dp[i - j];
     }
   }
-
   return dp[n];
-};
-
-/*************************** Solution2:  穷举  ************************************/
-let memo;
-
-var numTrees = function (n) {
-  memo = Array(n + 1)
-    .fill(0)
-    .map(() => Array(n + 1).fill(0)); // 备忘录的值初始化为nxn矩阵
-
-  return count(1, n); // 计算闭区间 [1, n] 组成的 BST 个数
-};
-
-//helper function: 计算闭区间 [lo, hi] 组成的 BST 个数
-const count = (low, high) => {
-  if (low > high) return 1;
-  // 查备忘录
-  if (memo[low][high] !== 0) return memo[low][high];
-
-  let res = 0;
-  for (let i = low; i <= high; i++) {
-    //i的值作为跟节点
-    let left = count(low, i - 1);
-    let right = count(i + 1, high);
-
-    // 左右子树的组合数乘积是 BST 的总数
-    res += left * right;
-  }
-  // 将结果存入备忘录
-  memo[low][high] = res;
-
-  return res;
 };
