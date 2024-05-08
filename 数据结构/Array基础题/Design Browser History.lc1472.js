c37d5c939732f9ae9d4819a96e5f9446152227ec
Sun flower <MLIU2@LTU.EDU>
2024-05-08 12:08:43 -0400

    add new

---
+++ 数据结构/Array基础题/Design Browser History.lc1472.js
@@ -0,0 +1,79 @@
+/****************************************** Solution 1: ES5 ******************************************************/
+/**
+ * @param {string} homepage
+ */
+var BrowserHistory = function (homepage) {
+  this.history = [homepage || ''];
+  this.curIdx = 0;
+};
+
+/**
+ * @param {string} url
+ * @return {void}
+ */
+BrowserHistory.prototype.visit = function (url) {
+  //重点: 截断历史记录数组。这样做可以确保只保留当前页面及之前的历史记录，
+  if (this.curIdx !== this.history.length - 1) {
+    this.history = this.history.slice(0, this.curIdx + 1); // 因为slice(start, end) 截出来的不包括end
+  }
+
+  this.history.push(url);
+  this.curIdx++;
+};
+
+/**
+ * @param {number} steps
+ * @return {string}
+ */
+BrowserHistory.prototype.back = function (steps) {
+  this.curIdx = Math.max(0, this.curIdx - steps); // curIdx最小值应该是0
+  return this.history[this.curIdx];
+};
+
+/**
+ * @param {number} steps
+ * @return {string}
+ */
+BrowserHistory.prototype.forward = function (steps) {
+  this.curIdx = Math.min(this.history.length - 1, this.curIdx + steps); // curIdx最大值应该是history.length - 1
+  return this.history[this.curIdx];
+};
+
+/*************************************** Solution 2: ES6 - class ***************************************************/
+class BrowserHistory {
+  constructor(homepage) {
+    this.history = [homepage || ''];
+    this.curIdx = 0;
+  }
+
+  /**
+   * @param {string} url
+   * @return {void}
+   */
+  visit(url) {
+    //重点: 截断历史记录数组。这样做可以确保只保留当前页面及之前的历史记录，
+    if (this.curIdx !== this.history.length - 1) {
+      this.history = this.history.slice(0, this.curIdx + 1);
+    }
+    this.history.push(url);
+    this.curIdx += 1;
+  }
+
+  /**
+   * @param {number} steps
+   * @return {string}
+   */
+  back(steps) {
+    this.curIdx = Math.max(0, this.curIdx - steps);
+    return this.history[this.curIdx];
+  }
+
+  /**
+   * @param {number} steps
+   * @return {string}
+   */
+  forward(steps) {
+    this.curIdx = Math.min(this.history.length - 1, this.curIdx + steps);
+    return this.history[this.curIdx];
+  }
+}
