327b0cd34e976bdc16630526b623aff0590ad11f
Sun flower <MLIU2@LTU.EDU>
2024-06-05 14:37:52 -0400

    232

--- 数据结构/Stack&Queue/ImplementQueueByUseStack.lc232.js
+++ 数据结构/Stack&Queue/ImplementQueueByUseStack.lc232.js
@@ -1,6 +1,6 @@
 // https://www.bilibili.com/video/BV1nY4y1w7VC/
 
-// ------------------------------------------------使用2个stack实现queue---------------------------------------------------------------------
+/* ------------------------------------------- 使用2个stack实现queue--------------------------------------- */
 var MyQueue = function () {
   this.stackIn = []; // 存储进入队列的元素
   this.stackOut = []; // 存储弹出队列的元素
@@ -20,16 +20,16 @@ MyQueue.prototype.push = function (x) {
  */
 // 从队列的开头删除元素（从 stackOut 中弹出）
 MyQueue.prototype.pop = function () {
-  // stackOut有值时 直接return this.stackOut.pop()
+  //case 1: stackOut中有值时 直接return this.stackOut.pop()
   if (this.stackOut.length) {
     return this.stackOut.pop();
   }
 
-  //stackOut无值时 把stackIn所有元素都要放到stackOut中
+  //case 2: stackOut中无值时 把stackIn所有元素都要放到stackOut中
   while (this.stackIn.length) {
     this.stackOut.push(this.stackIn.pop());
   }
-  // 然后从 stackOut 中弹出元素
+  //然后从 stackOut 中弹出元素
   return this.stackOut.pop();
 };
 
@@ -38,9 +38,11 @@ MyQueue.prototype.pop = function () {
  */
 // 返回队列的开头元素，但不删除它
 MyQueue.prototype.peek = function () {
-  const x = this.pop(); // 先从队列中弹出元素
-  this.stackOut.push(x); // 再将该元素推回队列
-  return x; // 返回该元素
+  //case 1: stackOut中有值时 返回stackOut中最后一项
+  if (this.stackOut.length) return this.stackOut[this.stackOut.length - 1];
+
+  //case 2: stackOut中无值时 返回stackIn的第一项
+  return this.stackIn[0];
 };
 
 /**
@@ -57,12 +59,3 @@ MyQueue.prototype.empty = function () {
 MyQueue.prototype.size = function () {
   return this.stackIn.length + this.stackOut.length;
 };
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * var obj = new MyQueue()
- * obj.push(x)
- * var param_2 = obj.pop()
- * var param_3 = obj.peek()
- * var param_4 = obj.empty()
- */
