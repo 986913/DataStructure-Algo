70bdee614b9a58e1140205d34d045df410c2aa12
Sun flower <MLIU2@LTU.EDU>
2024-06-26 21:59:45 -0400

    add new

---
+++ 数据结构/Stack&Queue/MonotonicQueue_单调队列_题目/Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.lc1438.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+/**************************** Solution1:  slding window 解法：O(n*k) *********************************************/
+
+/*************************** Solution2: Mono quque单调队列 + Sliding window *******************************
+
+  单调队列 - 是一个递增或递减的队列，可以用来维护窗口区间的最值，即RMQ问题
+  https://www.bilibili.com/video/BV1XS4y1p7qj/?vd_source=8b5297d974f6a5e72c60ec8ea33f2ff6
+*/
+
+/***********************  自定义的monoQueue ********************/
--- 算法思想/2 Pointers/同向2Pointers/快慢指针 - Sliding Window/固定窗口/Primitive window - Sliding Window Maximum.lc239.js
+++ 算法思想/2 Pointers/同向2Pointers/快慢指针 - Sliding Window/固定窗口/Primitive window - Sliding Window Maximum.lc239.js
@@ -25,7 +25,7 @@ var maxSlidingWindow = function (nums, k) {
   return result;
 };
 
-/*************************** Solution2: Mono quque单调队列 *******************************
+/*************************** Solution2: Mono quque单调队列 + Sliding window *******************************
 
   单调队列 - 是一个递增或递减的队列，可以用来维护窗口区间的最值，即RMQ问题
   https://www.bilibili.com/video/BV1XS4y1p7qj/?vd_source=8b5297d974f6a5e72c60ec8ea33f2ff6
