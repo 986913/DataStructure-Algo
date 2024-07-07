53b238c32cebffed518ee255cbecbd9e9ec741aa
Sun flower <MLIU2@LTU.EDU>
2024-07-07 15:37:54 -0400

    add new

---
+++ 算法思想/Hashtable 技巧/findDuplicates.lc442.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+var findDuplicates = function (nums) {
+  let map = new Map();
+  let arr = [];
+  nums.forEach((n) => {
+    if (!map.has(n)) map.set(n, 1);
+    else arr.push(n);
+  });
+  return arr;
+};
+
+var findDuplicates = function (nums) {
+  let map = new Map();
+  let arr = [];
+
+  nums.forEach((n) => {
+    if (map.has(n)) {
+      map.set(n, map.get(n) + 1);
+    } else {
+      map.set(n, 1);
+    }
+  });
+  /* iterate through a map in JS, can use forEach */
+  map.forEach((value, key) => {
+    if (value === 2) arr.push(key);
+  });
+
+  return arr;
+};
