d298ab4a779da21964002862133aab650d31e911
Sun flower <MLIU2@LTU.EDU>
2024-08-12 10:05:54 -0400

    1020

--- 算法思想/Backtracking/矩阵问题/岛屿问题/ Number of Enclaves.lc1020.js
+++ 算法思想/Backtracking/矩阵问题/岛屿问题/ Number of Enclaves.lc1020.js
@@ -10,7 +10,6 @@
 var numEnclaves = function (grid) {
   let m = grid.length;
   let n = grid[0].length;
-  let area = 0; // <--- diff is here
 
   // diff is here --> 把2D grid边界上的岛屿0都淹掉
   for (let i = 0; i < m; i++) {
@@ -23,15 +22,17 @@ var numEnclaves = function (grid) {
     }
   }
 
+  let area = 0; // <--- diff is here
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (grid[i][j] === 1) {
-        area += dfs(grid, i, j);
+        area += 1;
       }
     }
   }
   return area;
 };
+
 // helper function:
 const dfs = (matrix, i, j) => {
   let m = matrix.length;
@@ -41,13 +42,10 @@ const dfs = (matrix, i, j) => {
   if (matrix[i][j] === 0) return 0; //已经被淹过或者本来就是水 面积就是0
 
   matrix[i][j] = 0;
-  let area = 1; // <--- diff is here
-
-  let upArea = dfs(matrix, i - 1, j);
-  let downArea = dfs(matrix, i + 1, j);
-  let leftArea = dfs(matrix, i, j - 1);
-  let rightArea = dfs(matrix, i, j + 1);
-  return area + upArea + downArea + leftArea + rightArea; // <--- diff is here 有返回值了 返回累加的岛屿面积
+  dfs(matrix, i - 1, j);
+  dfs(matrix, i + 1, j);
+  dfs(matrix, i, j - 1);
+  dfs(matrix, i, j + 1);
 };
 
 /************************************ Solution: BFS - LC200,LC1254 BFS模版变形题 **************************************/
@@ -55,9 +53,8 @@ const dfs = (matrix, i, j) => {
 var numEnclaves = function (grid) {
   let m = grid.length;
   let n = grid[0].length;
-  let area = 0; // <--- diff is here
 
-  // diff is here --> 把2D grid边界上的岛屿0都淹掉
+  // diff is here --> 把2D grid边界上的岛屿都淹掉
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (i === 0 || i === m - 1 || j === 0 || j === n - 1) {
@@ -68,10 +65,11 @@ var numEnclaves = function (grid) {
     }
   }
 
+  let area = 0; // <--- diff is here
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (grid[i][j] === 1) {
-        area += bfs(grid, i, j);
+        area += 1;
       }
     }
   }
@@ -82,7 +80,6 @@ var numEnclaves = function (grid) {
 const bfs = (matrix, i, j) => {
   let m = matrix.length;
   let n = matrix[0].length;
-  let area = 1; // <-- diff is here
 
   let queue = [[i, j]]; //加进去的不是root了 而是[i，j]坐标
   matrix[i][j] = 0; //把岛屿淹掉
@@ -95,27 +92,21 @@ const bfs = (matrix, i, j) => {
 
       //淹没上下左右的陆地: 加进去不再是node.left/right/children了，而是x,y坐标的上下左右
       if (x - 1 >= 0 && matrix[x - 1][y] === 1) {
-        area += 1; // <-- diff is here
         queue.push([x - 1, y]);
         matrix[x - 1][y] = 0;
       }
       if (x + 1 < m && matrix[x + 1][y] === 1) {
-        area += 1; // <-- diff is here
         queue.push([x + 1, y]);
         matrix[x + 1][y] = 0;
       }
       if (y - 1 >= 0 && matrix[x][y - 1] === 1) {
-        area += 1; // <-- diff is here
         queue.push([x, y - 1]);
         matrix[x][y - 1] = 0;
       }
       if (y + 1 < n && matrix[x][y + 1] === 1) {
-        area += 1; // <-- diff is here
         queue.push([x, y + 1]);
         matrix[x][y + 1] = 0;
       }
     }
   }
-
-  return area; // <-- diff is here
 };
