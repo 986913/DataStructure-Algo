3ee72f1b27baf3c730418932fe2a534dad4b40f9
Sun flower <MLIU2@LTU.EDU>
2024-08-14 14:04:27 -0400

    add new

--- 算法思想/DFS/DFS in 矩阵/岛屿问题/Rotting Oranges.lc994.js
+++ 算法思想/DFS/DFS in 矩阵/岛屿问题/Rotting Oranges.lc994.js
@@ -4,55 +4,55 @@
  */
 
 /************************ Solution: BFS 推荐 **************************/
-svar orangesRotting = function(grid) {
-    let m = grid.length;
-    let n = grid[0].length;
+const orangesRotting = (grid) => {
+  let m = grid.length;
+  let n = grid[0].length;
 
-    let queue = [];
-    let freshOranges = 0; // <-- diff is here
+  let queue = [];
+  let freshOranges = 0; // <-- diff is here
 
-    for(let i=0; i<m; i++){
-        for(let j=0; j<n; j++){
-            if(grid[i][j]===2){
-                queue.push([i,j])
-            }else if(grid[i][j]===1){
-                freshOranges++  // <-- diff is here
-            }
-        }
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === 2) {
+        queue.push([i, j]);
+      } else if (grid[i][j] === 1) {
+        freshOranges++; // <-- diff is here
+      }
     }
+  }
 
-    let level = 0;  // <-- diff is here
-    while(queue.length){
-        let len = queue.length;
-        level+=1
+  let level = 0; // <-- diff is here
+  while (queue.length) {
+    let len = queue.length;
+    level += 1;
 
-        for(let i=0; i<len; i++){
-            let [x,y] = queue.shift();
+    for (let i = 0; i < len; i++) {
+      let [x, y] = queue.shift();
 
-            // 遍历四个方向: 上下左右
-            if (x - 1 >= 0 && grid[x - 1][y] === 1) {
-                grid[x - 1][y] = 2; //橘子变坏
-                queue.push([x - 1, y]); //更新queue
-                freshOranges--; // 更新freshOranges数量
-            }
-            if (x + 1 < m && grid[x + 1][y] === 1) {
-                grid[x + 1][y] = 2
-                queue.push([x + 1, y]);
-                freshOranges--;
-            }
-            if (y - 1 >= 0 && grid[x][y - 1] === 1) {
-                grid[x][y - 1] = 2
-                queue.push([x, y - 1]);
-                freshOranges--;
-            }
-            if (y + 1 < n && grid[x][y + 1] === 1) {
-                grid[x][y + 1] = 2
-                queue.push([x, y + 1]);
-                freshOranges--;
-            }
-        }
+      // 遍历四个方向: 上下左右
+      if (x - 1 >= 0 && grid[x - 1][y] === 1) {
+        grid[x - 1][y] = 2; //橘子变坏
+        queue.push([x - 1, y]); //更新queue
+        freshOranges--; // 更新freshOranges数量
+      }
+      if (x + 1 < m && grid[x + 1][y] === 1) {
+        grid[x + 1][y] = 2;
+        queue.push([x + 1, y]);
+        freshOranges--;
+      }
+      if (y - 1 >= 0 && grid[x][y - 1] === 1) {
+        grid[x][y - 1] = 2;
+        queue.push([x, y - 1]);
+        freshOranges--;
+      }
+      if (y + 1 < n && grid[x][y + 1] === 1) {
+        grid[x][y + 1] = 2;
+        queue.push([x, y + 1]);
+        freshOranges--;
+      }
     }
+  }
 
-    if(level===0 && freshOranges==0) return 0 // for the input case :  [[0]].
-    return freshOranges ===0 ? level-1 : -1  // <-- diff is here
-}; 
+  if (level === 0 && freshOranges == 0) return 0; // for the input case :  [[0]].
+  return freshOranges === 0 ? level - 1 : -1; // <-- diff is here
+};
