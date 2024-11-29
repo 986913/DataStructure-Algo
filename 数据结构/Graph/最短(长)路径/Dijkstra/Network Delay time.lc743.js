76fd5d26fcaba94262e08ca76f37d3076210d9a7
Sun flower <MLIU2@LTU.EDU>
2024-11-29 15:11:14 -0500

    1631

--- 数据结构/Graph/最短(长)路径/Dijkstra/Network Delay time.lc743.js
+++ 数据结构/Graph/最短(长)路径/Dijkstra/Network Delay time.lc743.js
@@ -35,6 +35,7 @@ const buildGraph = (n, edges) => {
   }
   return graph;
 };
+
 class State {
   constructor(id, distFromStart) {
     this.id = id; // 图节点的 id
@@ -70,3 +71,50 @@ const dijkstra = (graph, startIdx) => {
 
   return distTo;
 };
+
+class MyPriorityQueue {
+  constructor(compare) {
+    if (typeof compare !== 'function') {
+      throw new Error('compare function required!');
+    }
+
+    this.data = [];
+    this.compare = compare;
+  }
+
+  //二分查找 寻找插入位置
+  search(target) {
+    let low = 0,
+      high = this.data.length;
+    while (low < high) {
+      let mid = low + ((high - low) >> 1);
+      if (this.compare(this.data[mid], target) > 0) {
+        high = mid;
+      } else {
+        low = mid + 1;
+      }
+    }
+    return low;
+  }
+
+  //添加
+  enqueue(elem) {
+    let index = this.search(elem);
+    this.data.splice(index, 0, elem);
+    return this.data.length;
+  }
+
+  //取出最优元素
+  dequeue() {
+    return this.data.shift();
+  }
+
+  //查看最优元素
+  peek() {
+    return this.data[0];
+  }
+
+  isEmpty() {
+    return this.data.length === 0;
+  }
+}
