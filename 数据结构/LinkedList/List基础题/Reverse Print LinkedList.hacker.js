c6c56c46ffe8eb40febf8abcd91c8bc91f2fdcf9
Sun flower <MLIU2@LTU.EDU>
2025-12-31 17:19:32 -0500

    add comments

--- 数据结构/LinkedList/List基础题/Reverse Print LinkedList.hacker.js
+++ 数据结构/LinkedList/List基础题/Reverse Print LinkedList.hacker.js
@@ -1,3 +1,17 @@
+/**
+ * Problem Statement:
+ * Given the head of a singly linked list, print the elements in reverse order.
+ * You must not modify the original linked list.
+ *
+ * Example 1:
+ *   head = 1 -> 2 -> 3 -> null
+ *   Output: 3 2 1
+ *
+ * Example 2:
+ *   head = 5 -> 10 -> null
+ *   Output: 10 5
+ */
+
 /*********************************** Solution ***********************************
   时间 O(n)，因为每个节点只访问一次
   空间 O(n)，来自递归调用栈深度，而不是额外的数据结构
