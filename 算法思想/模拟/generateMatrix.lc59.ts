55dd13b91312a812e9e498ded517a40e52e3d2ae
Sun flower <MLIU2@LTU.EDU>
2026-07-27 17:20:36 -0400

    chore: update new prob

--- 算法思想/模拟/generateMatrix.lc59.ts
+++ 算法思想/模拟/generateMatrix.lc59.ts
@@ -67,3 +67,41 @@ function generateMatrix(n: number): number[][] {
 
   return matrix;
 }
+
+/******************************* Python Version ***********************************
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        matrix = [[0 for _ in range(n)] for _ in range(n)]
+
+        cur = 1
+
+        top = 0
+        bottom = n - 1
+        left = 0
+        right = n - 1
+
+        while top <= bottom and left <= right:
+            for j in range(left, right + 1):
+                matrix[top][j] = cur
+                cur += 1
+            top += 1
+
+            for i in range(top, bottom + 1):
+                matrix[i][right] = cur
+                cur += 1
+            right -= 1
+
+            if top <= bottom:
+                for j in range(right, left - 1, -1):
+                    matrix[bottom][j] = cur
+                    cur += 1
+                bottom -= 1
+
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    matrix[i][left] = cur
+                    cur += 1
+                left += 1
+
+        return matrix
+******************************* Python Version ***********************************/
